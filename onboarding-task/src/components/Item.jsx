15af723d00dca487efb1011ebaa5f8539e2f5343
PetrSvirak <petr.svirak@gmail.com>
2017-01-12 18:15:17 +0100

    Add propTypes for list item

--- onboarding-task/src/components/Item.jsx
+++ onboarding-task/src/components/Item.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 function Item(props){
   let { description, index } = props;
   return <li className="list-group-item">{index}. { description }</li>
 }
+
+Item.propTypes = {
+  description: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired
+};
 
 export default Item;
