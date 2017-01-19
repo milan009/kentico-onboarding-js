6b74bc41f7bc8431edb4b0fd2df0a768469a93d9
PetrSvirak <petr.svirak@gmail.com>
2017-01-19 16:44:11 +0100

    Convert DisplayItem to lambda function

--- onboarding-task/src/components/DisplayItem.jsx
+++ onboarding-task/src/components/DisplayItem.jsx
@@ -1,14 +1,15 @@
 import React, { PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 
-function DisplayItem(props) {
-  let { item: { description }, index } = props;
-  return (
-    <div onClick={() => props.onItemClick()}>
-      {index}. {description}
-    </div>
-  );
-}
+const DisplayItem = ({
+    item: { description },
+    index,
+    onItemClick,
+  }) => (
+  <div onClick={() => onItemClick()}>
+    {index}. {description}
+  </div>
+);
 
 DisplayItem.propTypes = {
   item: ImmutablePropTypes.recordOf({
