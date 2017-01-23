e95223e6200e2dc0b3b163fb3a8033dc7de15d9f
PetrSvirak <petr.svirak@gmail.com>
2017-01-23 21:44:16 +0100

    Fix app rendering test failing after redux interconnection

--- onboarding-task/test/App.test.js
+++ onboarding-task/test/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/App.jsx';
+import * as Redux from 'redux';
+import { Provider as ReduxProvider } from 'react-redux';
+import appReducer from '../src/reducers/app';
+import { seedStore } from '../src/utils/store';
 
 it('renders without crashing', () => {
+  const store = Redux.createStore(appReducer, seedStore());
+
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>,
+    div);
 });
