26d41f9922f3e5d0c2243d3e9e52ca3c2c2db756
PetrSvirak <petr.svirak@gmail.com>
2017-01-24 17:50:11 +0100

    Convert class ExistingItem to simpler function

--- onboarding-task/src/components/ExistingItem.jsx
+++ onboarding-task/src/components/ExistingItem.jsx
@@ -3,35 +3,33 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import DisplayItem from './DisplayItem';
 import EditItem from '../containers/EditItem';
 
-class ExistingItem extends Component {
-  static propTypes = {
-    index: PropTypes.number.isRequired,
-    item: ImmutablePropTypes.recordOf({
-      id: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-    }),
-    isEdited: PropTypes.bool.isRequired,
-    enableEdition: PropTypes.func.isRequired,
-  };
-
-  render() {
-    if (this.props.isEdited) {
-      return (
-        <EditItem
-          index={this.props.index}
-          item={this.props.item}
-        />
-      );
-    }
-
+const ExistingItem = props => {
+  if (props.isEdited) {
     return (
-      <DisplayItem
-        index={this.props.index}
-        onItemClick={this.props.enableEdition}
-        item={this.props.item}
+      <EditItem
+        index={props.index}
+        item={props.item}
       />
     );
   }
-}
+
+  return (
+    <DisplayItem
+      index={props.index}
+      onItemClick={props.enableEdition}
+      item={props.item}
+    />
+  );
+};
+
+ExistingItem.propTypes = {
+  index: PropTypes.number.isRequired,
+  item: ImmutablePropTypes.recordOf({
+    id: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  }),
+  isEdited: PropTypes.bool.isRequired,
+  enableEdition: PropTypes.func.isRequired,
+};
 
 export default ExistingItem;
