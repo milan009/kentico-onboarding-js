fbf0b5b17a612247cfeedb5e2fe6bca5746903d2
PetrSvirak <petr.svirak@gmail.com>
2017-01-24 18:30:59 +0100

    Fix items reducer being too complex for add action

--- onboarding-task/src/actions/addItem.js
+++ onboarding-task/src/actions/addItem.js
@@ -1,6 +1,7 @@
 import { ITEM_ADD } from './actionTypes';
 import action from './action';
+import Item from '../models/Item';
 
-const addItem = description => action(ITEM_ADD, { description });
+const addItem = description => action(ITEM_ADD, { item: new Item(description) });
 
 export default addItem;
--- onboarding-task/src/reducers/items.js
+++ onboarding-task/src/reducers/items.js
@@ -1,12 +1,10 @@
 import * as Immutable from 'immutable';
-import Item from '../models/Item';
 import { ITEM_ADD, ITEM_DELETE, ITEM_UPDATE_DESCRIPTION } from '../actions/actionTypes';
 
 function items(state = Immutable.OrderedMap(), action) {
   switch (action.type) {
     case ITEM_ADD: {
-      const newItem = new Item(action.description);
-      return state.set(newItem.id, newItem);
+      return state.set(action.item.id, action.item);
     }
     case ITEM_DELETE: {
       return state.delete(action.id);
