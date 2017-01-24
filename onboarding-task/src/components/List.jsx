1fe1ee49f4b331e2eb672ec0135a1c35ee5cefef
Ivan Jurina <ivanj@kentico.com>
2017-01-24 10:08:36 +0100

    Remove unnecessary assigment part

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -7,21 +7,6 @@ class List extends Component {
   render() {
     return (
       <div className="row">
-        {/* TODO: You can delete the assignment part once you do not need it */}
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <TsComponent name="Fancy" />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-sm-12">
-            <p className="lead text-center">Desired functionality is captured on the gif image. </p>
-            <p className="lead text-center"><b>Note: </b>Try to make solution easily extensible (e.g. more displayed fields per item).</p>
-            <img src={assignment} alt="assignment" className="img--assignment" />
-          </div>
-        </div>
-
         <div className="row">
           <div className="col-sm-12 col-md-offset-2 col-md-8">
             <pre>
