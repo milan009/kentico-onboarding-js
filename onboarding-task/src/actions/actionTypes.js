2cf888dc046cc037a357760c10fe0f85b84ded78
Ivan Jurina <ivanj@kentico.com>
2017-02-13 15:23:56 +0100

    Introduce actionTypes.js in src/actions

---
+++ onboarding-task/src/actions/actionTypes.js
@@ -0,0 +1,28 @@
+/**
+ * Created by IvanJ on 13.2.2017.
+ */
+import { generateGuid } from '../utils/utils.js';
+
+export const addItem = (text) => ({
+  type: 'ADD_ITEM',
+  guid: generateGuid(),
+  isEdited: false,
+  text,
+});
+
+export const toggleEditMode = (guid) => ({
+  type: 'TOGGLE_EDIT_MODE',
+  guid,
+});
+
+export const deleteItem = (guid) => ({
+  type: 'DELETE_ITEM',
+  guid,
+});
+
+export const updateItem = (guid, text) => ({
+  type: 'UPDATE_ITEM',
+  text,
+  guid,
+});
+
