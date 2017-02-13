bfe1b95c07791f69060ed2423ab99a5a4a3ae206
Pavel Fojtik <PavelF@kentico.com>
2017-02-13 15:00:53 +0100

    Task-1 - Refactor based on review

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import ListItem from './ListItem.jsx';
+import AddForm from './AddForm';
+import generateID from './../utils/idGenerator';
 
 class List extends Component {
   static displayName = 'List';
@@ -10,20 +12,18 @@ class List extends Component {
     this.state = {
       items: [],
     };
-    this._onAddClick = this._onAddClick.bind(this);
+    this._addItem = this._addItem.bind(this);
     this._deleteItem = this._deleteItem.bind(this);
     this._updateItem = this._updateItem.bind(this);
-    this._generateGUID = this._generateGUID.bind(this);
   }
 
-  _onAddClick() {
+  _addItem(text) {
     const newItem = {
-      id: this._generateGUID(),
-      text: this.itemText.value,
+      id: generateID(),
+      text,
     };
-    this.itemText.value = '';
     this.setState({
-      items: this.state.items.concat([newItem]),
+      items: [...this.state.items, newItem],
     });
   }
 
@@ -34,27 +34,17 @@ class List extends Component {
     });
   }
 
-  _updateItem(guid, text) {
+  _updateItem(id, text) {
     const updatedItems = this.state.items.map((item => {
-      const newItem = item;
-      if (item.id === guid) {
-        newItem.text = text;
+      if (item.id === id) {
+        const updatedItem = { ...item, text };
+        return updatedItem;
       }
-      return newItem;
+      return item;
     }));
     this.setState({
       items: updatedItems,
     });
-  }
-
-  _generateGUID() {
-    function s4() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-      s4() + '-' + s4() + s4() + s4();
   }
 
   render() {
@@ -63,19 +53,17 @@ class List extends Component {
         <div className="col-sm-12 col-md-offset-2 col-md-8">
           <pre>
             <ul className="list-group">
-              {this.state.items.map((item, index) => <ListItem text={item.text} index={index} delete={this._deleteItem} save={this._updateItem} key={item.id} id={item.id} />)}
+              {this.state.items.map((item, index) =>
+                <ListItem
+                  item={item}
+                  index={index}
+                  onDelete={this._deleteItem}
+                  onSave={this._updateItem}
+                  key={item.id}
+                />
+              )}
               <li className="list-group-item">
-                <form className="form-inline">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="itemText"
-                    ref={(input) => {
-                      this.itemText = input;
-                    }}
-                  />
-                  <button type="button" className="btn btn-default" onClick={this._onAddClick}>Add</button>
-                </form>
+                <AddForm onAdd={this._addItem} />
               </li>
             </ul>
           </pre>
--- onboarding-task/src/components/ListItem.jsx
+++ onboarding-task/src/components/ListItem.jsx
@@ -1,67 +1,51 @@
 import React, { Component, PropTypes } from 'react';
 
+import EditForm from './EditForm';
+
 class ListItem extends Component {
 
   static displayName = 'ListItem';
 
   static propTypes = {
-    text: PropTypes.string.isRequired,
-    delete: PropTypes.func.isRequired,
-    save: PropTypes.func.isRequired,
-    id: PropTypes.string.isRequired,
+    item: PropTypes.object.isRequired,
     index: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onSave: PropTypes.func.isRequired,
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      edit: false,
+      canEdit: false,
+      editInput: props.item.text,
     };
     this._startEditing = this._startEditing.bind(this);
     this._stopEditing = this._stopEditing.bind(this);
-    this._onSaveClick = this._onSaveClick.bind(this);
   }
 
   _startEditing() {
     this.setState({
-      edit: true,
+      canEdit: true,
     });
   }
 
   _stopEditing() {
     this.setState({
-      edit: false,
+      canEdit: false,
     });
   }
 
-  _onSaveClick() {
-    this.props.save(this.props.id, this.editInput.value);
-    this._stopEditing();
-  }
-
   render() {
-    if (this.state.edit) {
+    if (this.state.canEdit) {
       return (
         <li className="list-group-item">
-          <form className="form-inline">
-            <label>{`${this.props.index + 1}. `}</label>
-            <input
-              type="text"
-              className="form-control"
-              defaultValue={this.props.text} ref={(input) => {
-                this.editInput = input;
-              }}
-            />
-            <button type="button" className="btn btn-primary" onClick={this._onSaveClick}>Save</button>
-            <button type="button" className="btn btn-default" onClick={this._stopEditing} >Cancel</button>
-            <button type="button" className="btn btn-danger" onClick={() => this.props.delete(this.props.id)}>Delete</button>
-          </form>
+          <EditForm {...this.props} onCancel={this._stopEditing} />
         </li>
       );
     }
     return (
       <li className="list-group-item" onClick={this._startEditing} >
-        <span>{`${this.props.index + 1}. ${this.props.text}`}</span>
+        <span>{`${this.props.index + 1}. ${this.props.item.text}`}</span>
       </li>
     );
   }
