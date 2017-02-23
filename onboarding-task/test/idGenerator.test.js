0b430f3421a06d029546bda278a639665e0f3d3e
Pavel Fojtik <PavelF@kentico.com>
2017-02-23 14:33:08 +0100

    Task-3 - Add test for idGenerator

---
+++ onboarding-task/test/idGenerator.test.js
@@ -0,0 +1,8 @@
+import { generateId } from '../src/utils/idGenerator.js';
+
+describe('id Generator', () => {
+  it('should return id in specific format', () => {
+    const idFormatRE = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
+    expect(generateId()).toMatch(idFormatRE);
+  });
+});
