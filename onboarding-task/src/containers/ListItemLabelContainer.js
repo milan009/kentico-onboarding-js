b45a3a12870a517f3c09ade54f2b413fb1203a51
Jiri Lojda <JiriL@kentico.com>
2017-02-23 12:31:42 +0100

    Create ListItemLabelContainer

---
+++ onboarding-task/src/containers/ListItemLabelContainer.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+
+import { switchFormVisibilityForListItem } from '../actionCreators/actionCreators';
+import ListItemLabel from '../components/ListItemLabel';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    text: state.items.get(ownProps.id).text,
+    index: state.itemsOrder.indexOf(ownProps.id),
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClickHandler: () => switchFormVisibilityForListItem(ownProps.id),
+  };
+};
+
+const ListItemLabelContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ListItemLabel);
+
+export { ListItemLabelContainer };
