d9892016a9023bf3b7e161f64880c6efe8d17609
Jiri Lojda <JiriL@kentico.com>
2017-02-27 17:21:07 +0100

    KA-61 Refactor List to respect Redux and ListContainer

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -1,68 +1,31 @@
 import React, { Component } from 'react';
-import Immutable from 'immutable';
+import ImmutableProptypes from 'react-immutable-proptypes';
 
-import { ItemRecord } from '../models/ItemRecord';
 import ListItem from './ListItem';
 import CreateListItem from './CreateListItem';
-import guid from '../utils/guidHelper';
 
 class List extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: new Immutable.Map(),
-      itemsOrder: Immutable.List(),
-    };
-
-    this._updateListItemText = this._updateListItemText.bind(this);
-    this._addListItem = this._addListItem.bind(this);
-    this._deleteListItem = this._deleteListItem.bind(this);
-    this._switchListItemFormDisplayed = this._switchListItemFormDisplayed.bind(this);
-  }
-
-  _addListItem(text) {
-    const id = guid();
-    const newItem = new ItemRecord({ id, text, formDisplayed: false });
-    const newItems = this.state.items.set(id, newItem);
-    const newItemsOrder = this.state.itemsOrder.push(id);
-
-    this.setState({ items: newItems, itemsOrder: newItemsOrder });
-  }
-
-  _updateListItemText(id, text) {
-    const item = this.state.items.get(id);
-    const updatedItem = item.merge({ formDisplayed: false, text });
-    const newItems = this.state.items.set(id, updatedItem);
-
-    this.setState({ items: newItems });
-  }
-
-  _deleteListItem(id) {
-    const index = this.state.itemsOrder.indexOf(id);
-    const newItems = this.state.items.delete(id);
-    const newItemsOrder = this.state.itemsOrder.splice(index, 1);
-
-    this.setState({ items: newItems, itemsOrder: newItemsOrder });
-  }
-
-  _switchListItemFormDisplayed(id) {
-    const item = this.state.items.get(id);
-    const updatedItem = item.merge({ formDisplayed: !item.formDisplayed });
-    const newItems = this.state.items.set(id, updatedItem);
-
-    this.setState({ items: newItems });
-  }
+  static propTypes = {
+    items: ImmutableProptypes.mapOf(
+      ImmutableProptypes.recordOf({
+        id: React.PropTypes.string.isRequired,
+        text: React.PropTypes.string.isRequired,
+        formDisplayed: React.PropTypes.bool.isRequired,
+      }),
+      React.PropTypes.string.isRequired
+    ),
+    itemsOrder: ImmutableProptypes.listOf(
+      React.PropTypes.string.isRequired,
+    ),
+  };
 
   render() {
-    const listItems = this.state.itemsOrder.map((key, index) =>
+    const listItems = this.props.itemsOrder.map((key, index) =>
       <li key={key} className="list-group-item">
         <ListItem
-          index={index + 1}
-          item={this.state.items.get(key)}
-          onFormDisplayedSwitch={this._switchListItemFormDisplayed}
-          onDeleteClick={this._deleteListItem}
-          onFormSubmit={this._updateListItemText}
+          id={key}
+          formDisplayed={this.props.items.get(key).formDisplayed}
         />
       </li>
     );
@@ -71,7 +34,7 @@ class List extends Component {
       <ul className="list-group">
         {listItems}
         <li key="CreateListItemKey" className="list-group-item">
-          <CreateListItem onListItemAdd={this._addListItem} />
+          <CreateListItem />
         </li>
       </ul>
     );
