07f11e5e7a1502c252cd032d55c6a94d80c320bc
Jiri Lojda <JiriL@kentico.com>
2017-02-27 17:35:08 +0100

    KA-61 Create CreateListItemContainer

---
+++ onboarding-task/src/containers/CreateListItemContainer.js
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+
+import {createListItemFactory} from '../actionCreators/actionCreators';
+import CreateListItem from '../components/CreateListItem';
+import guid from '../utils/guidHelper';
+
+const createListItem = createListItemFactory(guid);
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onListItemAdd: (text) => dispatch(createListItem(text)),
+  };
+};
+
+const CreateListItemContainer = connect(
+  null,
+  mapDispatchToProps
+)(CreateListItem);
+
+export { CreateListItemContainer };
