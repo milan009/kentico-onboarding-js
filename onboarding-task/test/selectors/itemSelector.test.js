9fe514b5615396df1e4b1d7ae4f52d2ed197cb49
Pavel Fojtik <PavelF@kentico.com>
2017-02-28 08:33:17 +0100

    KA-55 - Create test for itemSelector

---
+++ onboarding-task/test/selectors/itemSelector.test.js
@@ -0,0 +1,40 @@
+import { OrderedMap, Set } from 'immutable';
+import { itemSelector } from '../../src/selectors/itemSelector.js';
+import { Item } from '../../src/models/ItemModel.js';
+
+describe('itemSelector', () => {
+  const testId = '0aeeaa2b-1a2a-482c-b2a6-b172109071e7';
+  const testText = 'test text';
+
+  it('should return true flag if item is edited', () => {
+    const expectedResult = {
+      id: testId,
+      text: testText,
+      isEdited: true,
+    };
+    const initialState = {
+      items: OrderedMap({ [testId]: Item({ id: testId, text: testText }) }),
+      editedItems: Set([testId]),
+    };
+
+    const selectorResult = itemSelector(initialState);
+
+    expect(selectorResult).toEqual(expectedResult);
+  });
+
+  it('should return false flag if item is not edited', () => {
+    const expectedResult = {
+      id: testId,
+      text: testText,
+      isEdited: false,
+    };
+    const initialState = {
+      items: OrderedMap({ [testId]: Item({ id: testId, text: testText }) }),
+      editedItems: Set(),
+    };
+
+    const selectorResult = itemSelector(initialState);
+
+    expect(selectorResult).toEqual(expectedResult);
+  });
+});
