0453572386b8ae5115a699dcb756314daaff97ad
Pavel Fojtik <PavelF@kentico.com>
2017-03-01 13:06:57 +0100

    KA-55 - Switch from reselect to memoize
    Both variant are correct, but memoize is standard in draft.

--- onboarding-task/src/selectors/itemSelector.js
+++ onboarding-task/src/selectors/itemSelector.js
@@ -1,17 +1,14 @@
-import { createSelector } from 'reselect';
-import { OrderedMap } from 'immutable';
+import memoize from 'memoizee';
 import { Item } from '../viewModels/Item.js';
 
 const getEditedItems = (state) => state.editedItems;
 const getItems = (state) => state.items;
 
-const itemSelector = createSelector(
-  [getEditedItems, getItems],
-  (editedItems, items) => {
-    return items.map((item, id) => {
-      return Item({ id, text: item.text, isEdited: editedItems.has(id) });
-    });
-  }
-);
+const itemSelector = memoize((state) => {
+  const editedItems = getEditedItems(state);
+  const items = getItems(state);
+  return items.map((item, id) => new Item({ id, text: item.text, isEdited: editedItems.has(id) }));
+});
+
 
 export { itemSelector };
