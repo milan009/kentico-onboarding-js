35a33e0a652582ae8f8f258b0a7a0dcef3194f41
Pavel Fojtik <PavelF@kentico.com>
2017-03-01 15:55:12 +0100

    KA-55 - Refactor getViewItems

--- onboarding-task/src/selectors/getViewItems.js
+++ onboarding-task/src/selectors/getViewItems.js
@@ -1,12 +1,9 @@
 import memoize from 'memoizee';
 import { ViewItem } from '../viewModels/ViewItem.js';
 
-const getEditedItems = (state) => state.editedItems;
-const getItems = (state) => state.items;
-
 const getViewItems = memoize((state) => {
-  const editedItems = getEditedItems(state);
-  const items = getItems(state);
+  const editedItems = state.editedItems;
+  const items = state.items;
   return items.map((item, id) => new ViewItem({ id, text: item.text, isEdited: editedItems.has(id) }));
 });
 
