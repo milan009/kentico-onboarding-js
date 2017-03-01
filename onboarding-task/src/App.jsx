a21ca11a21debb80b33d31ace0f1e8298499df39
David Cechak <DavidC@kentico.com>
2017-03-01 12:36:23 +0100

    Repair uncorrect import in App

--- onboarding-task/src/App.jsx
+++ onboarding-task/src/App.jsx
@@ -1,8 +1,8 @@
 import './sticky-footer.css';
-import React, { Component } from 'react';
-import List from './components/List.tsx';
+import * as React from 'react';
+import { List } from './components/List.tsx';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div>
