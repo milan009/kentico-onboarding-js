373439a8c3260c707e91ff93e5ce5bab5949d2b2
Ivan Jurina <ivanj@kentico.com>
2017-03-01 10:13:07 +0100

    KA-49 Rename guidGenerator parameter to generateGuid

--- onboarding-task/src/actions/actionDependencies/addItemFactory.js
+++ onboarding-task/src/actions/actionDependencies/addItemFactory.js
@@ -1,17 +1,14 @@
-/**
- * Created by IvanJ on 27.2.2017.
- */
 import { ADD_ITEM } from '../actionTypes.js';
 
-const addItem = (text, guidGenerator) => ({
+const addItem = (text, generateGuid) => ({
   type: ADD_ITEM,
   payload: {
-    guid: guidGenerator(),
+    guid: generateGuid(),
     isEdited: false,
     text,
   },
 });
 
-export function addItemFactory(guidGenerator) {
-  return (text) => addItem(text, guidGenerator);
+export function addItemFactory(generateGuid) {
+  return (text) => addItem(text, generateGuid);
 }
