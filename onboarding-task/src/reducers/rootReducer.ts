cec0243afdee846baba22fbdba3e90d368a23d10
Jiri Lojda <JiriL@kentico.com>
2017-03-06 17:51:25 +0100

    KA-62 Refactor rootReducer to use TypeScript

---
+++ onboarding-task/src/reducers/rootReducer.ts
@@ -0,0 +1,19 @@
+import { combineReducers } from 'redux';
+
+import { itemsReducer } from './itemsReducers/itemsReducer';
+import { IItems } from '../interfaces/IItems';
+import { IAction } from '../interfaces/IAction';
+
+interface IState {
+  items: IItems;
+}
+
+interface IRootReducer {
+  (state: IState, action: IAction): IState;
+}
+
+const rootReducer: IRootReducer = combineReducers({
+  items: itemsReducer,
+}) as IRootReducer;
+
+export { rootReducer };
