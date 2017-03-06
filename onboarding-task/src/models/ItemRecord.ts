eafe71e5484b2baa2d043b1f4be14171e0395951
Jiri Lojda <JiriL@kentico.com>
2017-03-06 15:04:57 +0100

    KA-62 Refactor ItemRecord to use separate interface for itemDefaultValues

--- onboarding-task/src/models/ItemRecord.ts
+++ onboarding-task/src/models/ItemRecord.ts
@@ -1,16 +1,21 @@
 import { Record } from 'immutable';
 
 interface IItemRecord {
+  id: string;
+  text: string;
+}
+
+interface IItemDefaultValues {
   id?: string;
   text?: string;
 }
 
-const defaultValues: IItemRecord = {
+const itemDefaultValues: IItemDefaultValues = {
   id: undefined,
   text: undefined,
 };
 
-class ItemRecord extends Record(defaultValues) implements IItemRecord {
+class ItemRecord extends Record(itemDefaultValues) implements IItemRecord {
   id: string;
   text: string;
 }
