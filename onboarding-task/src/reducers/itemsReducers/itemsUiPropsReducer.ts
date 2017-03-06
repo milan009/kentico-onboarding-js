2e969f56cee679d973626e892b8bca4688df26ab
Jiri Lojda <JiriL@kentico.com>
2017-03-06 17:20:21 +0100

    KA-62 Refactor itemsUiPropsReducer to use TypeScript

---
+++ onboarding-task/src/reducers/itemsReducers/itemsUiPropsReducer.ts
@@ -0,0 +1,25 @@
+import { Map } from 'immutable';
+
+import { ItemUiPropsRecord } from '../../models/ItemUiPropsRecord';
+import {
+  CREATE_ITEM_IN_LIST,
+  SWITCH_FORM_VISIBILITY_FOR_ITEM,
+  DELETE_ITEM_FROM_LIST,
+} from '../../constants/actionTypes';
+import { IAction } from '../../interfaces/IAction';
+
+const itemsUiPropsReducer = (prevState = Map<string, ItemUiPropsRecord>(), action: IAction) => {
+  switch (action.type) {
+    case CREATE_ITEM_IN_LIST:
+      return prevState.set(action.payload.id, new ItemUiPropsRecord());
+    case SWITCH_FORM_VISIBILITY_FOR_ITEM:
+      const formDisplayed = prevState.get(action.payload.id).formDisplayed;
+      return prevState.setIn([action.payload.id, 'formDisplayed'], formDisplayed);
+    case DELETE_ITEM_FROM_LIST:
+      return prevState.delete(action.payload.id);
+    default:
+      return prevState;
+  }
+};
+
+export { itemsUiPropsReducer };
