0524a82ed834ac43bd02b9ae0f31ee6fd4ec707f
Jiri Lojda <JiriL@kentico.com>
2017-03-06 17:48:08 +0100

    KA-62 Create IItems interface describing items object in state

--- onboarding-task/src/reducers/itemsReducers/itemsReducer.ts
+++ onboarding-task/src/reducers/itemsReducers/itemsReducer.ts
@@ -1,19 +1,10 @@
 import { combineReducers } from 'redux';
-import { Map, List} from 'immutable';
 
 import { itemsByIdsReducer } from './itemsByIdsReducer';
 import { itemsOrderReducer } from './itemsOrderReducer';
 import { itemsUiPropsReducer } from './itemsUiPropsReducer';
-import {} from '../../interfaces/IAction';
-import { ItemUiPropsRecord } from '../../models/ItemUiPropsRecord';
-import { ItemRecord } from '../../models/ItemRecord';
-import {IAction} from '../../interfaces/IAction';
-
-interface IItems {
-  byId: Map<string, ItemRecord>;
-  orderedIds: List<string>;
-  uiPropsById: Map<string, ItemUiPropsRecord>;
-}
+import { IItems } from '../../interfaces/IItems';
+import { IAction } from '../../interfaces/IAction';
 
 interface IItemsReducer {
   (itemsState: IItems, action: IAction): IItems;
