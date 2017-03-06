b66ac16727b425f78912005140409f09840db1b4
Jiri Lojda <JiriL@kentico.com>
2017-03-06 15:34:33 +0100

    KA-62 Refactor itemReducer to use TypeScript

--- onboarding-task/src/reducers/itemsReducers/itemReducer.ts
+++ onboarding-task/src/reducers/itemsReducers/itemReducer.ts
@@ -2,15 +2,19 @@ import {
   UPDATE_TEXT_OF_ITEM,
   CREATE_ITEM_IN_LIST,
 } from '../../constants/actionTypes';
-import { ItemRecord } from '../../models/ItemRecord.ts';
+import { ItemRecord } from '../../models/ItemRecord';
 
-const itemReducer = (prevState = new ItemRecord(), action) => {
+interface IAction {
+  type: string;
+  payload: { id: string; text?: string; };
+}
+
+const itemReducer = (prevState = new ItemRecord(), action: IAction) => {
   switch (action.type) {
     case CREATE_ITEM_IN_LIST:
       return new ItemRecord({ id: action.payload.id, text: action.payload.text, formDisplayed: false });
-    case UPDATE_TEXT_OF_ITEM: {
+    case UPDATE_TEXT_OF_ITEM:
       return prevState.merge({ text: action.payload.text });
-    }
     default:
       return prevState;
   }
