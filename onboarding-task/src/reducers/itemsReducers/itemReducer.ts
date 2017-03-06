ee24759c81a3691344bf03ec2323a6f63f19ff29
Jiri Lojda <JiriL@kentico.com>
2017-03-06 17:28:23 +0100

    KA-62 Refactor itemsByIdsReducer to use more specific types and return type

--- onboarding-task/src/reducers/itemsReducers/itemReducer.ts
+++ onboarding-task/src/reducers/itemsReducers/itemReducer.ts
@@ -5,12 +5,12 @@ import {
 import { ItemRecord } from '../../models/ItemRecord';
 import { IAction } from '../../interfaces/IAction';
 
-const itemReducer = (prevState = new ItemRecord(), action: IAction) => {
+const itemReducer = (prevState = new ItemRecord(), action: IAction): ItemRecord => {
   switch (action.type) {
     case CREATE_ITEM_IN_LIST:
       return new ItemRecord({ id: action.payload.id, text: action.payload.text, formDisplayed: false });
     case UPDATE_TEXT_OF_ITEM:
-      return prevState.merge({ text: action.payload.text });
+      return prevState.merge({ text: action.payload.text }) as ItemRecord;
     default:
       return prevState;
   }
--- onboarding-task/src/reducers/itemsReducers/itemsByIdsReducer.ts
+++ onboarding-task/src/reducers/itemsReducers/itemsByIdsReducer.ts
@@ -9,19 +9,14 @@ import { itemReducer } from './itemReducer';
 import {ItemRecord} from '../../models/ItemRecord';
 import { IAction } from '../../interfaces/IAction';
 
-interface IItemRecord extends ItemRecord {
-  id: string;
-  text: string;
-}
-
-const itemsByIdsReducer = (prevState = Map(), action: IAction) => {
+const itemsByIdsReducer = (prevState = Map<string, ItemRecord>(), action: IAction): Map<string, ItemRecord> => {
   switch (action.type) {
     case CREATE_ITEM_IN_LIST:
       const newItem = itemReducer(undefined, action);
       return prevState.set(action.payload.id, newItem);
     case UPDATE_TEXT_OF_ITEM:
       const item = prevState.get(action.payload.id);
-      return prevState.set(action.payload.id, itemReducer(item as IItemRecord, action));
+      return prevState.set(action.payload.id, itemReducer(item, action));
     case DELETE_ITEM_FROM_LIST:
       return prevState.delete(action.payload.id);
     default:
