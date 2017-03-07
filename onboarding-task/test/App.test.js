8666d20aa3021e7a70c9dff89b5c074d05334d60
Jiri Lojda <JiriL@kentico.com>
2017-03-07 16:51:34 +0100

    KA-62 Update references in App and App.test

--- onboarding-task/test/App.test.js
+++ onboarding-task/test/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { rootReducer } from '../src/reducers/rootReducer';
+import { rootReducer } from '../src/reducers/rootReducer.ts';
 import createLogger from 'redux-logger';
 
 import App from '../src/App.jsx';
