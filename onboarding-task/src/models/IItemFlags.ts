de2f8532accdf7631b566d518a675a739dfea2c9
David Cechak <DavidC@kentico.com>
2017-03-08 17:04:44 +0100

    Introduce ItemFlags record + Flags map in ListItem

---
+++ onboarding-task/src/models/IItemFlags.ts
@@ -0,0 +1,15 @@
+import { Record } from 'immutable';
+
+interface IItemFlags {
+  readonly editMode: boolean;
+}
+
+const defaultValues: IItemFlags = {
+  editMode: false,
+};
+
+class ItemFlags extends Record(defaultValues) implements IItemFlags {
+  readonly editMode: boolean;
+}
+
+export { ItemFlags, IItemFlags };
