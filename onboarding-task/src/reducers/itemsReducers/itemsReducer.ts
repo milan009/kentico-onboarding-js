8f5b3194b11387a61f3f759b6f4004d48eac0b21
Jiri Lojda <JiriL@kentico.com>
2017-03-13 18:27:05 +0100

    KA-105 Create isFetching reducer in itemsReducer

--- onboarding-task/src/reducers/itemsReducers/itemsReducer.ts
+++ onboarding-task/src/reducers/itemsReducers/itemsReducer.ts
@@ -5,13 +5,17 @@ import { itemsOrderReducer } from './itemsOrderReducer';
 import { itemsUiPropsReducer } from './itemsUiPropsReducer';
 import { IItems } from '../../interfaces/IItems';
 import { IAction } from '../../interfaces/IAction';
+import { FETCH_ITEMS_REQUEST } from '../../constants/actionTypes';
 
 type IItemsReducer = (itemsState: IItems, action: IAction) => IItems;
+
+const isFetchingReducer = (_: boolean, action: IAction) => action.type === FETCH_ITEMS_REQUEST;
 
 const itemsReducer: IItemsReducer = combineReducers({
   byId: itemsByIdsReducer,
   orderedIds: itemsOrderReducer,
   uiPropsById: itemsUiPropsReducer,
+  isFetching: isFetchingReducer,
 }) as IItemsReducer;
 
 export { itemsReducer };
