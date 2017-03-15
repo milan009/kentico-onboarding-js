d398ff6bffb65a93b44f83424d899da398d6ab4c
Jiri Lojda <JiriL@kentico.com>
2017-03-15 16:39:19 +0100

    KA-105 Create tests for fetchItems action creator

--- onboarding-task/test/actionCreators/fetchItemsActionCreators.test.js
+++ onboarding-task/test/actionCreators/fetchItemsActionCreators.test.js
@@ -1,13 +1,14 @@
 import {
   fetchItemsFailure,
   fetchItemsRequest,
-  fetchItemsSuccess
+  fetchItemsSuccess,
 } from '../../src/actionCreators/fetchItemsActionCreators.ts';
 import {
   FETCH_ITEMS_FAILURE,
   FETCH_ITEMS_REQUEST,
-  FETCH_ITEMS_SUCCESS
+  FETCH_ITEMS_SUCCESS,
 } from '../../src/constants/actionTypes.ts';
+import { fetchItemsFactory } from '../../src/actionCreators/fetchItemsFactories.ts';
 
 describe('fetchItemsActionCreators ', () => {
   it(`${fetchItemsRequest} returns correct ${FETCH_ITEMS_REQUEST} action`, () => {
@@ -29,5 +30,69 @@ describe('fetchItemsActionCreators ', () => {
     const actualAction = fetchItemsFailure('test');
 
     expect(actualAction).toEqual(expectedAction);
+  });
+});
+
+
+describe('fetchItems ', () => {
+  const getAllItemsResponse = [
+    {
+      id: '7383243d-9230-4a6c-94ea-122e151208ca',
+      text: 'text1',
+    },
+    {
+      id: '83aa9154-2b5f-49b7-b7af-25cab7bf2159',
+      text: 'text2',
+    },
+  ];
+
+  const fakeFetch = (path) => {
+    return path === '/api/Items'
+      ? Promise.resolve({ json: () => Promise.resolve(getAllItemsResponse) })
+      : Promise.reject();
+  };
+  const fetchItemsActionCreator = fetchItemsFactory(fakeFetch);
+
+  it('run without failure', (done) => {
+    fetchItemsActionCreator()((action) => action)
+      .then(() => done());
+  });
+
+  it('dispatches fetchItemsRequest', () => {
+    const mockDispatch = jest.fn((action) => action);
+    fetchItemsActionCreator()(mockDispatch);
+    const actualDispatchedAction = mockDispatch.mock.calls[0][0];
+
+    expect(actualDispatchedAction).toEqual(fetchItemsRequest());
+  });
+
+  it('calls fetchParam with correct path', (done) => {
+    const mockDispatch = action => action;
+    fetchItemsActionCreator()(mockDispatch).then(() => done());
+  });
+
+  it('dispatches fetchItemsSuccess with parsed response as argument when fetchParam returns Promise that resolves', (done) => {
+    const mockDispatch = jest.fn(action => action);
+    fetchItemsActionCreator()(mockDispatch).then(() => {
+      const actualDispatchedAction = mockDispatch.mock.calls[1][0];
+
+      expect(actualDispatchedAction).toEqual(fetchItemsSuccess(getAllItemsResponse));
+      done();
+    });
+  });
+
+  it('dispatches fetchItemsFailure with error message from the server as argument when fetchParam returns Promise that rejects', (done) => {
+    const mockDispatch = jest.fn(action => action);
+    const errorMessage = 'error Test';
+    const fakeFetchWithError = () => {
+      return Promise.reject({ text: () => errorMessage });
+    };
+    const fetchItemsActionCreatorWithError = fetchItemsFactory(fakeFetchWithError);
+    fetchItemsActionCreatorWithError()(mockDispatch).then(() => {
+      const actualDispatchedAction = mockDispatch.mock.calls[1][0];
+
+      expect(actualDispatchedAction).toEqual(fetchItemsFailure(errorMessage));
+      done();
+    });
   });
 });
