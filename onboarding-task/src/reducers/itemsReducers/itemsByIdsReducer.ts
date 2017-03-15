4380f496193a0bb23c28504cf908606308ebb541
Jiri Lojda <JiriL@kentico.com>
2017-03-15 16:57:21 +0100

    KA-105 Add case into itemsByIdsReducer handling FETCH_ITEMS_SUCCESS action

--- onboarding-task/src/reducers/itemsReducers/itemsByIdsReducer.ts
+++ onboarding-task/src/reducers/itemsReducers/itemsByIdsReducer.ts
@@ -4,10 +4,12 @@ import {
   UPDATE_TEXT_OF_ITEM,
   CREATE_ITEM_IN_LIST,
   DELETE_ITEM_FROM_LIST,
+  FETCH_ITEMS_SUCCESS,
 } from '../../constants/actionTypes';
 import { itemReducer } from './itemReducer';
 import {ItemRecord} from '../../models/ItemRecord';
 import { IAction } from '../../interfaces/IAction';
+import { IFetchedItem } from '../../interfaces/IFetchedItem';
 
 const itemsByIdsReducer = (prevState = Map<string, ItemRecord>(), action: IAction): Map<string, ItemRecord> => {
   switch (action.type) {
@@ -21,6 +23,13 @@ const itemsByIdsReducer = (prevState = Map<string, ItemRecord>(), action: IActio
 
     case DELETE_ITEM_FROM_LIST:
       return prevState.delete(action.payload.id);
+
+    case FETCH_ITEMS_SUCCESS:
+      let result = Map<string, ItemRecord>();
+      action.payload.response.forEach((fetchedItem: IFetchedItem) => {
+        result = result.set(fetchedItem.id, new ItemRecord({ id: fetchedItem.id, text: fetchedItem.text }));
+      });
+      return result;
 
     default:
       return prevState;
