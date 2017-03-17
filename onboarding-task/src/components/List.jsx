d5269a0068ee47bc36e721d57d20a2f51a4a4c24
Linda Hanslikova <LindaH@kentico.com>
2017-03-17 10:21:13 +0100

    KA-109 - Main visuals added: toggling text fields and buttons. Save and Cancel button with functionality.

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -1,37 +1,183 @@
 import React, { Component } from 'react';
 import assignment from './../../../assignment.gif';
 
-import TsComponent from './TsComponent.tsx';
-
 class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lines: [],
+    };
+    this.handleAddLine = this.handleAddLine.bind(this);
+  }
+  handleAddLine(event) {
+    this.setState({ lines: event.target });
+    let data = this.state.data;
+    const id = this.generateId().toString();
+    const complete = 'false';
+    data = data.concat([{ id, event, complete }]);
+    this.setState({ data });
+  }
+
   render() {
     return (
       <div className="row">
-        {/* TODO: You can delete the assignment part once you do not need it */}
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <TsComponent name="𝕱𝖆𝖓𝖈𝖞" />
-          </div>
-        </div>
-
         <div className="row">
           <div className="col-sm-12">
-            <p className="lead text-center">Desired functionality is captured on the gif image. </p>
-            <p className="lead text-center"><b>Note: </b>Try to make solution easily extensible (e.g. more displayed fields per item).</p>
+            <p className="lead text-center">The desired functionality is captured in the gif image. </p>
+            <p className="lead text-center"><b>Note: </b>Try to make the solution easily extensible (e.g. more displayed fields per item).</p>
             <img src={assignment} alt="assignment" className="img--assignment" />
           </div>
         </div>
 
         <div className="row">
           <div className="col-sm-12 col-md-offset-2 col-md-8">
-            <pre>
-              // TODO: implement the list here :)
-            </pre>
+              {/* // TODO: implement the list here :) */}
+            <ul id="todo-list" className="list-group">
+              <Line />
+              <li className="list-group-item">
+                <input />
+                <button type="button" className="btn btn-default" onClick={this.handleAddLine}>Add</button>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     );
   }
+}
+
+class Line extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEdited: false,
+      text: 'superb',
+      prevText: 'superb',
+      number: props.number,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClickSave = this.handleClickSave.bind(this);
+    this.handleClickCancel = this.handleClickCancel.bind(this);
+    this.handleDoubleClick = this.handleDoubleClick.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ text: event.target.value });
+  }
+  handleClickSave(event) {
+    this.setState(
+      {
+        isEdited: false,
+        prevText: this.state.text,
+      }
+    );
+  }
+  handleClickCancel(event) {
+    this.setState(
+      {
+        isEdited: false,
+        text: this.state.prevText,
+      }
+    );
+  }
+
+  handleDoubleClick(event) {
+    this.setState(
+      {
+        isEdited: true,
+      }
+    );
+  }
+
+  render() {
+    return (
+      <li className="list-group-item" onDoubleClick={this.handleDoubleClick} >
+        <span> {this.state.number} </span>
+        <LineText isEdited={this.state.isEdited} text={this.state.text} onChange={this.handleChange} />
+        {this.state.isEdited === true ?
+          <span>
+            <button type="button" className="btn btn-primary" onClick={this.handleClickSave} >Save</button>
+            <button type="button" className="btn btn-default" onClick={this.handleClickCancel} >Cancel</button>
+            <button type="button" className="btn btn-danger">Delete</button>
+          </span> : null}
+      </li>
+    );
+  }
+}
+
+Line.propTypes = {
+  number: React.PropTypes.number.isRequired,
+};
+
+function LineText(props) {
+  const isEdited = props.isEdited;
+  if (isEdited) {
+    return (
+      <input className="" value={props.text} onChange={props.onChange} />
+    );
+  }
+  return <span onDoubleClick={props.onDoubleClick}> {props.text} </span>;
+}
+
+LineText.propTypes = {
+  isEdited: React.PropTypes.bool.isRequired,
+  text: React.PropTypes.string.isRequired,
+  onChange: React.PropTypes.isRequired,
+  onDoubleClick: React.PropTypes.isRequired,
+};
+
+
+/* function LineText(props) {
+ const isEdited = props.isEdited;
+ let type = isEdited ? 'input' : 'text';
+ return <input type={type}> Make coffee </input>;
+ } */
+
+/*
+class Line extends Component {
+  render() {
+      let aline = null;
+      <li className="list-group-item">
+        this.id
+        if(isClicked) {
+          return <TodoInput />
+      } else {
+          return <TodoText text=this.text />
+      }
+
+      </li>
+  }
+}
+
+function TodoText(props){
+  return(
+    <p>props.text;</p>
+  );
+}
+
+function TodoInput(props){
+  return(
+    <input />
+    <button type="button" className="btn btn-default btn-primary">
+    Save
+    </button>
+    <button type="button" className="btn btn-default">
+    Cancel
+    </button>
+    <button type="button" className="btn btn-default btn-danger">
+    Delete
+    </button>
+  );
+}
+*/
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
 
 export default List;
