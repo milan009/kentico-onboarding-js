91230c41899ad26e26576787fce8f3e114456dc6
Jiri Lojda <JiriL@kentico.com>
2017-03-20 14:09:55 +0100

    KA-105 Create sendItemFactory, sendItem dispatched redux-thunk

---
+++ onboarding-task/src/actionCreators/sendItemFactory.ts
@@ -0,0 +1,20 @@
+import TsPromise from 'ts-promise';
+require('isomorphic-fetch');
+
+import { IAction } from '../interfaces/IAction';
+import { IFetchedItem } from '../interfaces/IFetchedItem';
+import { dispatchType } from '../utils/dispatchType';
+import { sendItemSuccess, sendItemFailure } from './sendItemActionCreators';
+import { fetchType } from '../utils/fetchType';
+
+const sendItem = (fetchParam: fetchType, item: IFetchedItem) => {
+  return (dispatch: dispatchType) =>
+    fetchParam('/api/Items', { method: 'POST', body: item })
+      .then<IFetchedItem | string>((response: Response) => response.ok ? response.json() : TsPromise.reject(new Error(response.statusText)))
+      .then<IAction>((json: IFetchedItem) => dispatch(sendItemSuccess(json)))
+      .catch<IAction>((error: Error) => dispatch(sendItemFailure(error.message)));
+};
+
+const sendItemFactory = (fetchParam: fetchType) => (item: IFetchedItem) => sendItem(fetchParam, item);
+
+export { sendItemFactory };
