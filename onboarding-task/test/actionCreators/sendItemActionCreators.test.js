0be668d5132473f305af66e3b743c8ccc6424da7
Jiri Lojda <JiriL@kentico.com>
2017-03-20 13:46:45 +0100

    KA-105 Create tests for sendItem action creator

--- onboarding-task/test/actionCreators/sendItemActionCreators.test.js
+++ onboarding-task/test/actionCreators/sendItemActionCreators.test.js
@@ -28,3 +28,44 @@ describe('sendItemActionCreators ', () => {
     expect(actualAction).toEqual(expectedAction);
   });
 });
+
+describe('sendItem ', () => {
+  const fakeSuccessResponse = [{ Id: 'id', Value: 'text' }];
+  const successMessage = `Item ${fakeSuccessResponse.Value} was successfully uploaded.`;
+  const fakeFetch = (path) => {
+    return path === '/api/Items'
+      ? Promise.resolve({ json: () => Promise.resolve(fakeSuccessResponse), ok: true })
+      : Promise.reject();
+  };
+  const sendItemActionCreator = sendItemFactory(fakeFetch);
+
+  it('calls fetchParam with correct path', (done) => {
+    const mockDispatch = action => action;
+    sendItemActionCreator()(mockDispatch).then(() => done());
+  });
+
+  it('dispatches fetchItemsSuccess with parsed response as argument when fetchParam returns Promise that resolves', (done) => {
+    const mockDispatch = jest.fn(action => action);
+    sendItemActionCreator()(mockDispatch).then(() => {
+      const actualDispatchedAction = mockDispatch.mock.calls[0][0];
+
+      expect(actualDispatchedAction).toEqual(sendItemSuccess(successMessage));
+      done();
+    });
+  });
+
+  it('dispatches fetchItemsFailure with error message from the server as argument when fetchParam returns Promise that rejects', (done) => {
+    const mockDispatch = jest.fn(action => action);
+    const errorMessage = 'error Test';
+    const fakeFetchWithError = () => {
+      return Promise.resolve({ statusText: errorMessage, ok: false });
+    };
+    const fetchItemsActionCreatorWithError = sendItemFactory(fakeFetchWithError);
+    fetchItemsActionCreatorWithError()(mockDispatch).then(() => {
+      const actualDispatchedAction = mockDispatch.mock.calls[0][0];
+
+      expect(actualDispatchedAction).toEqual(sendItemFailure(errorMessage));
+      done();
+    });
+  });
+});
