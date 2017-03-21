bd6b5264be7463c64bd4e4c759296ad63e054b49
David Cechak <DavidC@kentico.com>
2017-03-21 14:17:53 +0100

    Create save editted item - test & reducer

---
+++ onboarding-task/src/reducers/saveItemEditReducer.ts
@@ -0,0 +1,22 @@
+import * as Immutable from 'immutable';
+
+import {ITEM_EDITED} from '../actions/actionTypes';
+import {Item} from '../models/Item';
+import {ItemFlags} from '../models/ItemFlags';
+import {editItem, toggleItemViewMode} from '../actions/actionCreators';
+import {editItemValueReducer} from './editItemValueReducer';
+import {toggleItemViewModeReducer} from './toggleItemViewModeReducer';
+
+const saveItemEditReducer = (state: { items: Immutable.Map<string,Item>, itemsDisplayFlags: Immutable.Map<string,ItemFlags> }, action: any) => {
+  switch (action.type) {
+    case ITEM_EDITED: {
+      state.items = editItemValueReducer(state.items, editItem(action.id, action.value));
+      state.itemsDisplayFlags = toggleItemViewModeReducer(state.itemsDisplayFlags, toggleItemViewMode(action.id));
+      return state;
+    }
+    default:
+      return state;
+  }
+};
+
+export { saveItemEditReducer };
--- onboarding-task/test/reducers/reducers.test.js
+++ onboarding-task/test/reducers/reducers.test.js
@@ -5,6 +5,7 @@ import {Item} from '../../src/models/Item.ts';
 import {toggleItemViewMode, editItem} from '../../src/actions/actionCreators';
 import {toggleItemViewModeReducer} from '../../src/reducers/toggleItemViewModeReducer';
 import {editItemValueReducer} from '../../src/reducers/editItemValueReducer';
+import {saveItemEditReducer} from '../../src/reducers/saveItemEditReducer';
 
 describe('toggle item edit view mode', () => {
   it('switch to edit mode', () => {
@@ -19,7 +20,7 @@ describe('toggle item edit view mode', () => {
 
     const actualState = toggleItemViewModeReducer(state, toggleItemViewMode(id));
     expect(actualState.get(id).editMode).toEqual(true);
-  })
+  });
 });
 
 describe('edit item value', () => {
@@ -36,6 +37,31 @@ describe('edit item value', () => {
 
     const actualState = editItemValueReducer(state, editItem(id, 'expected text'));
     expect(expectedNewState).toEqual(actualState);
-  })
+  });
 });
 
+describe('save item edit', () => {
+  it('set new value for existing item and toggle view mode to label(= editMode: false)', () => {
+    const id = 'da5cbf5f-2d20-4945-b8d2-4cc3b6be1542';
+    const item = new Item({
+      id,
+      value: 'old text'
+    });
+    const itemFlags = new ItemFlags({
+      id,
+      editMode: true
+    });
+    const items = Immutable.Map().set(id, item);
+    const displayFlags = Immutable.Map().set(id, itemFlags);
+    const expectedText = 'expected text';
+
+    const state = {items: items, itemsDisplayFlags: displayFlags};
+    const expectedState = {
+      items: items.setIn([id, 'value'], expectedText),
+      itemsDisplayFlags: displayFlags.setIn([id, 'editMode'], false)
+    };
+
+    const actualState = saveItemEditReducer(state, editItem(id, expectedText));
+    expect(expectedState).toEqual(actualState);
+  });
+});
