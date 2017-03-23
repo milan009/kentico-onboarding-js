1555366fcb306c22f5cd71c2fc7c8905c327d34a
Tobias Kamenicky <TobiasK@kentico.com>
2017-03-23 11:49:46 +0100

    KA-100 Add test for itemValidator

--- onboarding-task/src/utils/itemValidator.js
+++ onboarding-task/src/utils/itemValidator.js
@@ -3,6 +3,16 @@ function validateItemText(text) {  // might be part of validateItem() later, if 
     isValid: true,
     messages: [],
   };
+  if (text === null) {
+    error.isValid = false;
+    error.messages = [...error.messages, 'Value cannot be null'];
+    return error;
+  }
+  if (text === undefined) {
+    error.isValid = false;
+    error.messages = [...error.messages, 'Value cannot be undefined'];
+    return error;
+  }
   if (!text.match(/\S/)) {
     error.isValid = false;
     error.messages = [...error.messages, 'Item text should not be empty'];
---
+++ onboarding-task/test/utils/itemValidator.test.js
@@ -0,0 +1,24 @@
+
+import { validateItemText } from '../../src/utils/itemValidator.js';
+
+describe('Validate item text:', () => {
+  it('should be invalid for empty text', () => {
+    expect(validateItemText('').isValid).toBeFalsy();
+  });
+
+  it('should be invalid for null', () => {
+    expect(validateItemText(null).isValid).toBeFalsy();
+  });
+
+  it('should be invalid for undefined value', () => {
+    expect(validateItemText(undefined).isValid).toBeFalsy();
+  });
+
+  it('should be valid for non-empty text', () => {
+    expect(validateItemText('testing text').isValid).toBeTruthy();
+  });
+
+  it('should return error messages on error', () => {
+    expect(validateItemText('').messages.length).toBeGreaterThan(0);
+  });
+});
