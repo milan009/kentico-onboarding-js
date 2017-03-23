ae9bd71006fee5b7eb45c9a87009b6e71cb249c9
Tobias Kamenicky <TobiasK@kentico.com>
2017-03-23 08:09:00 +0100

    KA-100 Convert List to stateless component

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -1,44 +1,45 @@
-import React, { PureComponent } from 'react';
+import React, { PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import TsComponent from './TsComponent.tsx';
 import { CreateItem } from './CreateItem.jsx';
 import { ListRowContainer } from '../containers/ListRowContainer.js';
 
-class List extends PureComponent {
-  static displayName = 'List';
-  static propTypes = {
-    items: ImmutablePropTypes.map,
-  };
 
-  render() {
-    const listItems = this.props.items.valueSeq().map((item, i) => {
-      return (
-        <div key={item.id} className="list-group-item item-custom">
-          <ListRowContainer index={i + 1} id={item.id} />
-        </div>
-      );
-    });
-
+function List(props) {
+  const listItems = props.items.valueSeq().map((item, i) => {
     return (
+      <div key={item.id} className="list-group-item item-custom">
+        <ListRowContainer index={i + 1} id={item.id} />
+      </div>
+    );
+  });
+
+  return (
+    <div className="row">
       <div className="row">
-        <div className="row">
-          <div className="col-sm-12 text-center">
-            <TsComponent name="𝕱𝖆𝖓𝖈𝖞" />
-          </div>
+        <div className="col-sm-12 text-center">
+          <TsComponent name="𝕱𝖆𝖓𝖈𝖞" />
         </div>
-        <div className="row">
-          <div className="col-sm-12 col-md-offset-2 col-md-8">
-            <div className="list-group">
-              {listItems}
-              <div className="list-group-item">
-                <CreateItem onItemAdd={this.props.onItemAdd} />
-              </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-12 col-md-offset-2 col-md-8">
+          <div className="list-group">
+            {listItems}
+            <div className="list-group-item">
+              <CreateItem onItemAdd={props.onItemAdd} />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+List.displayName = 'List';
+
+List.propTypes = {
+  items: ImmutablePropTypes.map.isRequired,
+  onItemAdd: PropTypes.func.isRequired,
+};
 
 export { List };
