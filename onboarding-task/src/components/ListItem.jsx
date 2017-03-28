3c6e55d428289f22e6a8b0db6708246b7822e6d4
Linda Hanslikova <LindaH@kentico.com>
2017-03-28 11:06:52 +0200

    KA-110 - Change ListItem to a stateless functional component

--- onboarding-task/src/components/ListItem.jsx
+++ onboarding-task/src/components/ListItem.jsx
@@ -1,47 +1,39 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { LineEdit } from './LineEdit.jsx';
 import { LineRead } from './LineRead.jsx';
 
-class ListItem extends PureComponent {
-  static displayName = 'ListItem';
-
-  static propTypes = {
-    line: PropTypes.object,
-    index: PropTypes.number.isRequired,
-    onSave: PropTypes.func.isRequired,
-    onCancel: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired,
-    onDoubleClick: PropTypes.func.isRequired,
-  };
-
-  _renderLineItem = (line, index) => {
-    if (line.row.get('isEdited')) {
-      return (
-        <LineEdit
-          key={line.id}
-          line={line}
-          index={(index)}
-          onSave={this.props.onSave}
-          onCancel={this.props.onCancel}
-          onDelete={this.props.onDelete}
-        />
-      );
-    }
+const ListItem = (props) => {
+  if (props.line.row.get('isEdited')) {
     return (
-      <LineRead
-        key={line.id}
-        line={line}
-        index={(index)}
-        onDoubleClick={this.props.onDoubleClick}
+      <LineEdit
+        key={props.line.id}
+        line={props.line}
+        index={(props.index)}
+        onSave={props.onSave}
+        onCancel={props.onCancel}
+        onDelete={props.onDelete}
       />
     );
-  };
-
-  render() {
-    return (
-      this._renderLineItem(this.props.line, this.props.index)
-    );
   }
-}
+  return (
+    <LineRead
+      key={props.line.id}
+      line={props.line}
+      index={(props.index)}
+      onDoubleClick={props.onDoubleClick}
+    />
+  );
+};
+
+ListItem.displayName = 'ListItem';
+
+ListItem.propTypes = {
+  line: PropTypes.object.isRequired,
+  index: PropTypes.number.isRequired,
+  onSave: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onDoubleClick: PropTypes.func.isRequired,
+};
 
 export { ListItem };
