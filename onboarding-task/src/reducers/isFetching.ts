3a3b67e373a923779109ea7e03b32c1ed52a908b
Pavel Fojtik <PavelF@kentico.com>
2017-03-28 10:20:51 +0200

    KA-104 - Add isFetching reducer

---
+++ onboarding-task/src/reducers/isFetching.ts
@@ -0,0 +1,20 @@
+import {IAction} from '../actions/IAction';
+import {FETCH_ITEMS_FAIL, FETCH_ITEMS_RECEIVE, FETCH_ITEMS_REQUEST} from '../actions/actionTypes';
+
+const isFetching = (state = true, action: IAction) => {
+  switch (action.type) {
+    case FETCH_ITEMS_REQUEST:
+      return true;
+
+    case FETCH_ITEMS_RECEIVE:
+      return false;
+
+    case FETCH_ITEMS_FAIL:
+      return false;
+
+    default:
+      return state;
+  }
+};
+
+export { isFetching };
--- onboarding-task/src/reducers/listApp.ts
+++ onboarding-task/src/reducers/listApp.ts
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import { items } from './items';
 import { editedItems } from './editedItems';
 import { itemsOrder } from './itemsOrder';
+import {isFetching} from './isFetching';
 
 const listApp = combineReducers({
   items,
   editedItems,
   itemsOrder,
+  isFetching,
 });
 
 export { listApp };
