c617a7973bcc15339fb932254cbfa5b78defe0c1
David Cechak <DavidC@kentico.com>
2017-03-31 16:43:37 +0200

    KA-67 Create IAppState

---
+++ onboarding-task/src/stores/IAppState.ts
@@ -0,0 +1,12 @@
+import * as Immutable from 'immutable';
+import { Item } from '../models/Item';
+import { ItemFlags } from '../models/ItemFlags';
+
+
+interface IAppState {
+  items: Immutable.Map<string,Item>;
+  itemsOrder: Immutable.OrderedSet<string>;
+  itemsDisplayFlags: Immutable.Map<string, ItemFlags>;
+};
+
+export { IAppState };
