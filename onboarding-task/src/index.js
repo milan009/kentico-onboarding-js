b352f6f14501783d27f4751254c1d5b9a0cd6e08
David Cechak <DavidC@kentico.com>
2017-03-31 16:48:55 +0200

    KA-67 Use Provider in index

--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -4,7 +4,19 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
+
+import { itemsReducer } from './reducers/itemsReducer.ts';
+
+
+const logger = createLogger();
+const store = createStore(itemsReducer, applyMiddleware(logger));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />,
+  </Provider>,
   document.getElementById('app-root')
 );
