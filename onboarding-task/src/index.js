d97c6f48387ea9f6f9584efd2d96b2b68bb84cea
Linda Hanslikova <LindaH@kentico.com>
2017-04-03 12:03:16 +0200

    KA-111 - Create instance of Redux store with root reducer

--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import manageItems from './reducers/manageItems';
+// import todoAppReducers from './reducers/index.js';
+import { todoAppReducers } from './reducers/index.js';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+let store = createStore(todoAppReducers);
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app-root')
 );
---
+++ onboarding-task/src/reducers/index.js
@@ -0,0 +1,6 @@
+// import { combineReducers } from 'redux';
+import { manageItems } from './manageItems.js';
+
+const todoAppReducers = manageItems;
+
+export { todoAppReducers };
