b86e7d52666b1209a03db1de5bcd99de7337e508
Jiri Lojda <JiriL@kentico.com>
2017-04-03 15:36:23 +0200

    KA-105 Add colors into ListITemSavedFlag

--- onboarding-task/src/components/ListItemSavedFlag.tsx
+++ onboarding-task/src/components/ListItemSavedFlag.tsx
@@ -4,7 +4,11 @@ interface ListItemSavedFlagProps {
   saved: boolean;
 }
 
-const ListItemSavedFlag = ({ saved }: ListItemSavedFlagProps) =>
-  <span><span className={saved ? 'glyphicon glyphicon-cloud' : 'glyphicon glyphicon-alert'}></span></span>;
+const ListItemSavedFlag = ({ saved }: ListItemSavedFlagProps) => {
+  const style = {
+    color: saved ? 'green' : 'orange',
+  }
+  return <span><span className={saved ? 'glyphicon glyphicon-cloud' : 'glyphicon glyphicon-alert'} style={style}></span></span>;
+}
 
 export { ListItemSavedFlag };
