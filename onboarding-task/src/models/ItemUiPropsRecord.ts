e09244e80c7cb3e9eeec83fd04a85713f7d6d297
Jiri Lojda <JiriL@kentico.com>
2017-04-03 13:59:50 +0200

    KA-105 Add savedOnServer property into ItemUiPropsRecord

--- onboarding-task/src/models/ItemUiPropsRecord.ts
+++ onboarding-task/src/models/ItemUiPropsRecord.ts
@@ -2,18 +2,22 @@ import { Record } from 'immutable';
 
 interface IItemUiPropsRecord {
   formDisplayed: boolean;
+  savedOnServer: boolean;
 }
 
 interface IItemUiPropsDefaultValues {
   formDisplayed?: boolean;
+  savedOnServer?: boolean;
 }
 
 const itemUiPropsDefaultValues: IItemUiPropsDefaultValues = {
   formDisplayed: false,
+  savedOnServer: false,
 };
 
 class ItemUiPropsRecord extends Record(itemUiPropsDefaultValues) implements IItemUiPropsRecord {
   formDisplayed: boolean;
+  savedOnServer: boolean;
 }
 
 export { ItemUiPropsRecord };
