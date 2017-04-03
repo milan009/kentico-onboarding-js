a5cc967789f8e24cd73e5c3b188a0619278a6c5c
Tobias Kamenicky <TobiasK@kentico.com>
2017-04-03 08:21:25 +0200

    KA-101 Remove explicit type string from actionTypes

--- onboarding-task/src/actions/actionTypes.ts
+++ onboarding-task/src/actions/actionTypes.ts
@@ -1,7 +1,7 @@
-export const ITEM_CREATE: string = 'ITEM_CREATE';
+export const ITEM_CREATE = 'ITEM_CREATE';
 
-export const ITEM_UPDATE: string = 'ITEM_UPDATE';
+export const ITEM_UPDATE = 'ITEM_UPDATE';
 
-export const ITEM_DELETE: string = 'ITEM_DELETE';
+export const ITEM_DELETE = 'ITEM_DELETE';
 
-export const ITEM_TOGGLE_EDIT: string = 'ITEM_TOGGLE_EDIT';
+export const ITEM_TOGGLE_EDIT = 'ITEM_TOGGLE_EDIT';
