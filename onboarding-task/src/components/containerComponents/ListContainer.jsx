3838a8de1c76f70a5d158004b8722892d7cca832
Linda Hanslikova <LindaH@kentico.com>
2017-04-04 23:15:36 +0200

    KA-111 - Create ListContainer, List ready for refactoring

---
+++ onboarding-task/src/components/containerComponents/ListContainer.jsx
@@ -0,0 +1,38 @@
+import { connect } from 'react-redux';
+import { List } from '../List.jsx';
+import { addItem, editItem, deleteItem, saveChangesToItem, cancelChangesToItem } from '../../actionCreators/actionCreators.js';
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    _handleAddLine: (text) => {
+      dispatch(addItem(text));
+    },
+
+    _handleDeleteLine: (id) => {
+      dispatch(deleteItem(id));
+    },
+
+    _handleDoubleClick: (id) => {
+      dispatch(editItem(id));
+    },
+
+    _handleClickSave: (id, text) => {
+      dispatch(saveChangesToItem(id, text));
+    },
+
+    _handleClickCancel: (id) => {
+      dispatch(cancelChangesToItem(id));
+    },
+  };
+}
+
+const ListContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(List);
+
+export {ListContainer};
