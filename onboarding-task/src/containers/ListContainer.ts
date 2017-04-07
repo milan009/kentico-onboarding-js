87b097132b12c739b50424330f452b53c558c841
David Cechak <DavidC@kentico.com>
2017-04-07 14:55:25 +0200

    KA-67 Use own Dispatch in ListContainer

--- onboarding-task/src/containers/ListContainer.ts
+++ onboarding-task/src/containers/ListContainer.ts
@@ -2,10 +2,9 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import { List } from '../components/List';
-import { Dispatch } from 'redux';
+import { Dispatch } from '../stores/Dispatch';
 import { IAppState } from '../stores/IAppState';
 import { createItem, deleteItem, editItem, toggleItemViewMode } from '../actions/actionCreators';
-import { IAction } from '../stores/IAction';
 
 function mapStateToProps(state: IAppState) {
   return {
@@ -13,12 +12,12 @@ function mapStateToProps(state: IAppState) {
   };
 }
 
-function mapDispatchToProps(dispatch: Dispatch<IAction>) {
+function mapDispatchToProps(dispatch: Dispatch) {
   return {
     onAddItem: (value: string) => dispatch(createItem(value)),
     onEditItemValue: (id: string, value: string) => dispatch(editItem(id, value)),
     onDeleteItem: (id: string) => dispatch(deleteItem(id)),
-    onToggleItemViewMode: (id: string) => dispatch(toggleItemViewMode(id))
+    onToggleItemViewMode: (id: string) => dispatch(toggleItemViewMode(id)),
   };
 }
 
