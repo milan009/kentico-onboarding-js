546c5f29efc00435e80a95b136170e77b005f598
David Cechak <DavidC@kentico.com>
2017-04-09 16:02:28 +0200

    KA-67 Repair ListContainer to suit List
    Use List with ListContainer

--- onboarding-task/src/components/List.tsx
+++ onboarding-task/src/components/List.tsx
@@ -1,82 +1,33 @@
 import * as React from 'react';
 import * as Immutable from 'immutable';
 
-import { ListItem } from './ListItem';
+import { ListItem } from '../containers/ListItemContainer';
 import { AddItem } from './AddItem';
-import { generateGuid } from '../utils/generateGuid';
-import { Item } from '../models/Item';
-import { ItemFlags } from '../models/ItemFlags';
 
-interface IListState {
-  items: Immutable.Map<string,Item>;
+interface IListDataProps {
   itemsOrder: Immutable.OrderedSet<string>;
-  itemsDisplayFlags: Immutable.Map<string, ItemFlags>;
 }
 
-class List extends React.PureComponent<undefined, IListState> {
+interface IListCallbacksProps {
+  onAddItem: (value: string) => void,
+}
+
+class List extends React.PureComponent<IListDataProps & IListCallbacksProps, undefined> {
   static displayName = 'List';
 
-  constructor(props: undefined) {
+  static propTypes = {
+    itemsOrder: React.PropTypes.instanceOf(Immutable.OrderedSet).isRequired,
+    onAddItem: React.PropTypes.func.isRequired,
+  };
+
+  constructor(props: IListDataProps & IListCallbacksProps) {
     super(props);
-    this.state = {
-      items: Immutable.Map<string,Item>(),
-      itemsOrder: Immutable.OrderedSet<string>(),
-      itemsDisplayFlags: Immutable.Map<string, ItemFlags>()
-    };
   }
 
-  _addItem = (value: string) => {
-    const newItem = new Item({
-      id: generateGuid(),
-      value
-    });
-    const itemFlags = new ItemFlags({
-      editMode: false
-    });
-
-    this.setState({
-      items: this.state.items.set(newItem.id, newItem),
-      itemsOrder: this.state.itemsOrder.add(newItem.id),
-      itemsDisplayFlags: this.state.itemsDisplayFlags.set(newItem.id, itemFlags)
-    });
-  };
-
-  _editItemValue = (id: string, value: string) => {
-    const newItemsDisplayFlags = this.state.itemsDisplayFlags.setIn([id, 'editMode'], false);
-
-    this.setState({
-      items: this.state.items.setIn([id, 'value'], value),
-      itemsDisplayFlags: newItemsDisplayFlags
-    })
-  };
-
-  _deleteItem = (deletedItemId: string) => {
-    this.setState({
-      items: this.state.items.delete(deletedItemId),
-      itemsOrder: this.state.itemsOrder.delete(deletedItemId),
-      itemsDisplayFlags: this.state.itemsDisplayFlags.delete(deletedItemId)
-    });
-  };
-
-  _toggleItemViewMode = (id: string) => {
-    const newFlag = !this.state.itemsDisplayFlags.get(id).editMode;
-
-    const newItemsDisplayFlags = this.state.itemsDisplayFlags.setIn([id, 'editMode'], newFlag);
-
-    this.setState({ itemsDisplayFlags: newItemsDisplayFlags });
-  };
-
   _renderListItems = () => {
-    return this.state.itemsOrder.toIndexedSeq().map((id: string, zeroBasedIndex: number) =>
+    return this.props.itemsOrder.toIndexedSeq().map((id: string) =>
       <li className="list-group-item" key={id}>
-        <ListItem
-          item={this.state.items.get(id)}
-          index={zeroBasedIndex + 1}
-          isInEditMode={this.state.itemsDisplayFlags.get(id).editMode}
-          onItemValueEdit={this._editItemValue}
-          onDelete={this._deleteItem}
-          onViewChange={this._toggleItemViewMode}
-        />
+        <ListItem id={id} />
       </li>
     );
   };
@@ -87,7 +38,7 @@ class List extends React.PureComponent<undefined, IListState> {
         <ul className="list-group">
           {this._renderListItems()}
           <li className="list-group-item">
-            <AddItem onAdd={this._addItem} />
+            <AddItem onAdd={this.props.onAddItem} />
           </li>
         </ul>
       </div>
@@ -95,4 +46,4 @@ class List extends React.PureComponent<undefined, IListState> {
   }
 }
 
-export { List };
+export { List, IListDataProps, IListCallbacksProps };
--- onboarding-task/src/containers/ListContainer.ts
+++ onboarding-task/src/containers/ListContainer.ts
@@ -1,26 +1,23 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import { List } from '../components/List';
+import {IListCallbacksProps, IListDataProps, List} from '../components/List';
 import { Dispatch } from '../stores/Dispatch';
 import { IAppState } from '../stores/IAppState';
-import { createItem, deleteItem, editItem, toggleItemViewMode } from '../actions/actionCreators';
+import { createItem } from '../actions/actionCreators';
 
-function mapStateToProps(state: IAppState) {
+function mapStateToProps(state: IAppState) : IListDataProps {
   return {
       itemsOrder: state.itemsOrder,
   };
 }
 
-function mapDispatchToProps(dispatch: Dispatch) {
+function mapDispatchToProps(dispatch: Dispatch) : IListCallbacksProps {
   return {
     onAddItem: (value: string) => dispatch(createItem(value)),
-    onEditItemValue: (id: string, value: string) => dispatch(editItem(id, value)),
-    onDeleteItem: (id: string) => dispatch(deleteItem(id)),
-    onToggleItemViewMode: (id: string) => dispatch(toggleItemViewMode(id)),
   };
 }
 
-const ListContainer: React.ComponentClass<undefined> = connect(mapStateToProps, mapDispatchToProps)(List);
+const ListContainer: React.ComponentClass<{}> = connect(mapStateToProps, mapDispatchToProps)(List);
 
 export { ListContainer as List };
