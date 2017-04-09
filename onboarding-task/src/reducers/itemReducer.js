47af36f1d0e01fb35604d87cb274700700114b8b
LindaHa <lindaelvira.h@gmail.com>
2017-04-09 16:27:18 +0200

    KA-111 - Return new item in itemReducer.js instead of merging

--- onboarding-task/src/reducers/itemReducer.js
+++ onboarding-task/src/reducers/itemReducer.js
@@ -5,7 +5,7 @@ const itemReducer = (state = new Item(), action) => {
   switch (action.type) {
     case ADD_ITEM: {
       const item = new Item({ id: action.payload.id, text: action.payload.text, isEdited: false });
-      return state.merge(item);
+      return item;
     }
 
     case EDIT_ITEM: {
