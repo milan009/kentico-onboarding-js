b69431dd824a037466ce364a018ed5a90a8bab56
Jiri Lojda <JiriL@kentico.com>
2017-04-10 16:17:05 +0200

    KA-105 Refactor ErrorStatusMessage to be more readable

--- onboarding-task/src/components/utilComponents/StatusMessage.tsx
+++ onboarding-task/src/components/utilComponents/StatusMessage.tsx
@@ -2,16 +2,20 @@ import * as React from 'react';
 
 import { MessageType } from '../../utils/MessageType';
 
-const ErrorStatusMessage = ({ error }: { error: string }) => error && error !== ''
-  ? (<div className="alert alert-danger" role="alert">
-    <strong>Error!</strong> {error}
-  </div> )
+const ErrorStatusMessage = ({ error }: { error: string }) =>
+  error ? (
+    <div className="alert alert-danger" role="alert">
+      <strong>Error!</strong> {error}
+    </div>
+  )
   : <noscript />;
 
-const SuccessStatusMessage = ({ successMessage }: { successMessage: string }) => successMessage && successMessage !== ''
-  ? (<div className="alert alert-success" role="alert">
-    <strong>Success!</strong> {successMessage}
-  </div> )
+const SuccessStatusMessage = ({ successMessage }: { successMessage: string }) =>
+  successMessage ? (
+    <div className="alert alert-success" role="alert">
+      <strong>Success!</strong> {successMessage}
+    </div>
+  )
   : <noscript />;
 
 const StatusMessage = ({ message , messageType }: { message: string; messageType: MessageType }) => {
