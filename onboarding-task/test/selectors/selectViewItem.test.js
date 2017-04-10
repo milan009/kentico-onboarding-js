8e695b3d7a056883baac1406d2ca415c6694cf34
Pavel Fojtik <PavelF@kentico.com>
2017-04-10 15:32:44 +0200

    KA-104 - Fix selectViewItem tests

--- onboarding-task/test/selectors/selectViewItem.test.js
+++ onboarding-task/test/selectors/selectViewItem.test.js
@@ -1,6 +1,6 @@
-import { OrderedMap, Set } from 'immutable';
 import { selectViewItem } from '../../src/selectors/selectViewItem.ts';
 import { ViewItem } from '../../src/viewModels/ViewItem.ts';
+import { Item } from '../../src/models/Item.ts';
 
 describe('selectViewItem', () => {
   const testId = '0aeeaa2b-1a2a-482c-b2a6-b172109071e7';
@@ -8,24 +8,20 @@ describe('selectViewItem', () => {
 
   it('should return true flag if item is edited', () => {
     const expectedResult = new ViewItem({ id: testId, text: testText, isEdited: true, index: 1 });
-    const initialState = {
-      items: OrderedMap({ [testId]: new ViewItem({ id: testId, text: testText }) }),
-      editedItems: Set([testId]),
-    };
+    const testItem = new Item({ id: testId, text: testText });
+    const isEdited = true;
 
-    const selectorResult = selectViewItem(initialState, testId, 0);
+    const selectorResult = selectViewItem(testItem, isEdited, 0);
 
     expect(selectorResult).toEqual(expectedResult);
   });
 
   it('should return false flag if item is not edited', () => {
     const expectedResult = new ViewItem({ id: testId, text: testText, isEdited: false, index: 1 });
-    const initialState = {
-      items: OrderedMap({ [testId]: new ViewItem({ id: testId, text: testText }) }),
-      editedItems: Set(),
-    };
+    const testItem = new Item({ id: testId, text: testText });
+    const isEdited = false;
 
-    const selectorResult = selectViewItem(initialState, testId, 0);
+    const selectorResult = selectViewItem(testItem, isEdited, 0);
 
     expect(selectorResult).toEqual(expectedResult);
   });
