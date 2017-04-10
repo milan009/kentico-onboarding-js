380af142f8edda5e1044d58d19e498afc74a32b3
Jiri Lojda <JiriL@kentico.com>
2017-04-10 17:05:53 +0200

    KA-105 Refactor sendItemActionCreators.test

--- onboarding-task/test/actionCreators/sendItemActionCreators.test.js
+++ onboarding-task/test/actionCreators/sendItemActionCreators.test.js
@@ -35,45 +35,41 @@ describe('sendItemActionCreators ', () => {
 
 describe('sendItem ', () => {
   const fakeDispatch = action => action;
-  const fakeSuccessResponse = { id: 'id', value: 'text', ueid: 'ueid' };
+  const fakeItem = { id: 'id', value: 'text', ueid: 'ueid' };
   const fakeFetch = (path, params) => {
     return path === '/api/v1/Items'
-      ? Promise.resolve({ json: () => Promise.resolve(fakeSuccessResponse), ok: true })
+      ? Promise.resolve({ json: () => Promise.resolve(fakeItem), ok: true })
       : Promise.reject();
   };
   const sendItemActionCreator = sendItemFactory(fakeFetch);
 
   it('calls fetchParam with correct path', (done) => {
-    sendItemActionCreator(fakeSuccessResponse)(fakeDispatch).then(() => done());
+    sendItemActionCreator(fakeItem)(fakeDispatch).then(() => done());
   });
 
   it('calls fetchParam with correct parameters', (done) => {
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
-    const fetchParams = { method: 'POST', body: JSON.stringify(fakeSuccessResponse), headers: myHeaders };
-    const fakeFetchWithSpy = jest.fn((path) => {
-      return path === '/api/v1/Items'
-        ? Promise.resolve({ json: () => Promise.resolve(fakeSuccessResponse), ok: true })
-        : Promise.reject();
-    });
-    sendItemFactory(fakeFetchWithSpy)(fakeSuccessResponse)(fakeDispatch).then(() => {
+    const fetchParams = { method: 'POST', body: JSON.stringify(fakeItem), headers: myHeaders };
+    const fakeFetchWithSpy = jest.fn(fakeFetch);
+    sendItemFactory(fakeFetchWithSpy)(fakeItem)(fakeDispatch).then(() => {
       expect(fakeFetchWithSpy.mock.calls[0][1]).toEqual(fetchParams);
       done();
     });
   });
 
   it('dispatches sendItemSuccess with parsed response as argument when response.ok', (done) => {
-    const mockDispatch = jest.fn((action) => action);
-    sendItemActionCreator(fakeSuccessResponse)(mockDispatch).then(() => {
+    const mockDispatch = jest.fn(fakeDispatch);
+    sendItemActionCreator(fakeItem)(mockDispatch).then(() => {
       const actualDispatchedAction = mockDispatch.mock.calls[0][0];
 
-      expect(actualDispatchedAction).toEqual(sendItemSuccess(fakeSuccessResponse));
+      expect(actualDispatchedAction).toEqual(sendItemSuccess(fakeItem));
       done();
     });
   });
 
   it('dispatches sendItemFailure with error message from the server as argument when !response.ok', (done) => {
-    const mockDispatch = jest.fn((action) => action);
+    const mockDispatch = jest.fn(fakeDispatch);
     const errorMessage = 'error Test';
     const fakeFetchWithError = () => {
       return Promise.resolve({ statusText: errorMessage, ok: false });
