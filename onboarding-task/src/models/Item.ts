22bdcfe0f861f3a530c54f7ac20c9ac14ff8eed5
Pavel Fojtik <PavelF@kentico.com>
2017-04-11 08:58:25 +0200

    KA-104 - Add constructor implementing interfacae for item model

--- onboarding-task/src/models/Item.ts
+++ onboarding-task/src/models/Item.ts
@@ -5,6 +5,11 @@ interface IItem {
   readonly text: string;
 }
 
+interface IItemProps {
+  readonly id?: string;
+  readonly text?: string;
+}
+
 const defaultValues: IItem = {
   id: '00000000-0000-0000-0000-000000000000',
   text: '',
@@ -13,6 +18,10 @@ const defaultValues: IItem = {
 class Item extends Record(defaultValues) implements IItem {
   readonly id: string;
   readonly text: string;
+
+  constructor(props: IItemProps) {
+    super(props);
+  }
 }
 
 export { Item, IItem };
