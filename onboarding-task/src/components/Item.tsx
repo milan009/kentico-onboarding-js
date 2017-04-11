809cc5a5af128ae5836fe2aa3f23e5f966a7623e
Pavel Fojtik <PavelF@kentico.com>
2017-04-11 08:08:35 +0200

    KA-104 - Change Item.tsx to stateless component

--- onboarding-task/src/components/Item.tsx
+++ onboarding-task/src/components/Item.tsx
@@ -13,45 +13,27 @@ interface IItemProps {
   stopEditingItem: () => IAction;
 }
 
-class Item extends React.PureComponent<IItemProps, undefined> {
-  static displayName = 'Item';
+const Item: React.StatelessComponent<IItemProps> = ({ item, deleteItem, updateItem, startEditingItem, stopEditingItem }) => (
+  item.isEdited ?
+    <EditForm
+      item={item}
+      onSave={updateItem}
+      onDelete={deleteItem}
+      onCancel={stopEditingItem}
+    />
+  : <ItemDetail startEditingItem={startEditingItem} index={item.index} text={item.text} />
+);
 
-  static propTypes = {
-    item: ImmutablePropTypes.recordOf({
-      text: React.PropTypes.string,
-      isEdited: React.PropTypes.bool,
-      index: React.PropTypes.number,
-    }).isRequired,
-    deleteItem: React.PropTypes.func.isRequired,
-    updateItem: React.PropTypes.func.isRequired,
-    startEditingItem: React.PropTypes.func.isRequired,
-    stopEditingItem: React.PropTypes.func.isRequired,
-  };
-
-  constructor(props: IItemProps) {
-    super(props);
-    this._getItemToRender = this._getItemToRender.bind(this);
-  }
-
-  _getItemToRender(item: IViewItem) {
-    if (item.isEdited) {
-      return (
-        <EditForm
-          item={item}
-          onSave={this.props.updateItem}
-          onDelete={this.props.deleteItem}
-          onCancel={this.props.stopEditingItem}
-        />
-      );
-    }
-    return <ItemDetail startEditingItem={this.props.startEditingItem} index={item.index} text={item.text} />;
-  }
-
-  render() {
-    return (
-        this._getItemToRender(this.props.item)
-    );
-  }
-}
+Item.propTypes = {
+  item: ImmutablePropTypes.recordOf({
+    text: React.PropTypes.string,
+    isEdited: React.PropTypes.bool,
+    index: React.PropTypes.number,
+  }).isRequired,
+  deleteItem: React.PropTypes.func.isRequired,
+  updateItem: React.PropTypes.func.isRequired,
+  startEditingItem: React.PropTypes.func.isRequired,
+  stopEditingItem: React.PropTypes.func.isRequired,
+};
 
 export { Item };
