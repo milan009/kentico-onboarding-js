53f9ee1eedf97e14469211901cafaa9259c41f0d
David Cechak <DavidC@kentico.com>
2017-04-11 12:53:53 +0200

    KA-67 Create own dispatch

--- onboarding-task/src/stores/Dispatch.ts
+++ onboarding-task/src/stores/Dispatch.ts
@@ -1,8 +1,3 @@
-import { Dispatch } from 'react-redux';
-
 import { IAction } from '../actions/IAction';
 
-
-type TDispatch = Dispatch<(action: IAction) => IAction>;
-
-export { TDispatch as Dispatch};
+export type Dispatch = (action: IAction) => IAction;
