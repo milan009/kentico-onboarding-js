3e58224a664be61fd9ad7980b89702d10eaaad97
Linda Hanslikova <LindaH@kentico.com>
2017-04-11 10:01:58 +0200

    KA-111 - Fix itemIdsReducer.js (state is now in OrderedSet)

--- onboarding-task/test/reducerTests/itemIdsReducer.test.js
+++ onboarding-task/test/reducerTests/itemIdsReducer.test.js
@@ -1,34 +1,34 @@
-import { List } from 'immutable';
+import { OrderedSet } from 'immutable';
 import { ADD_ITEM, DELETE_ITEM } from '../../src/constants/actionTypes.js';
 import { itemIdsReducer } from '../../src/reducers/itemIdsReducer.js';
 
 describe('Correctly adds and removes item Ids: ', () => {
   const fakeId = '23456899-df58-1bbf-1j8p-4asd582z69d8';
   const text = 'text';
-  it('Reducer for adding a new Id into an unexisting List', () => {
+  it('Reducer for adding a new Id into an unexisting OrderedSet', () => {
     const action = { type: ADD_ITEM, payload: { id: fakeId, text } };
-    const expected = List.of(fakeId);
+    const expected = OrderedSet.of(fakeId);
 
     const tested = itemIdsReducer(undefined, action);
 
     expect(tested).toEqual(expected);
   });
 
-  it('Reducer for adding a new Id into a nonempty List', () => {
+  it('Reducer for adding a new Id into a nonempty OrderedSet', () => {
     const action = { type: ADD_ITEM, payload: { id: fakeId, text } };
     const firstId = 's54d8e2x-15e8-4s52-e44s-ad71e2d5zz40';
-    const initialState = List.of(firstId);
-    const expected = List.of(firstId, fakeId);
+    const initialState = OrderedSet.of(firstId);
+    const expected = OrderedSet.of(firstId, fakeId);
 
     const tested = itemIdsReducer(initialState, action);
-
+    console.log('expected: ', expected, 'tested: ', tested);
     expect(tested).toEqual(expected);
   });
 
   it('Reducer for deleting an Id', () => {
     const action = { type: DELETE_ITEM, payload: { id: fakeId } };
-    const initialState = List.of(fakeId);
-    const expected = List();
+    const initialState = OrderedSet.of(fakeId);
+    const expected = OrderedSet();
 
     const tested = itemIdsReducer(initialState, action);
 
@@ -37,7 +37,7 @@ describe('Correctly adds and removes item Ids: ', () => {
 
   it('Reducer with unknown action', () => {
     const action = { type: 'I AM UNKNOWN', payload: { id: fakeId } };
-    const initialState = List.of(fakeId);
+    const initialState = OrderedSet.of(fakeId);
 
     const tested = itemIdsReducer(initialState, action);
 
