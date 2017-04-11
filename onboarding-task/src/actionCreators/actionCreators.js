b97145c5cb849f5e102c9cb19280d10c40ed7c02
Linda Hanslikova <LindaH@kentico.com>
2017-04-11 09:48:34 +0200

    KA-111 - Put action type and payload are on separate lines

--- onboarding-task/src/actionCreators/actionCreators.js
+++ onboarding-task/src/actionCreators/actionCreators.js
@@ -5,17 +5,21 @@ import { createGuid } from '../utils/guidHelper.js';
 export const addItem = addItemFactory(createGuid);
 
 export const deleteItem = (id) => ({
-  type: DELETE_ITEM, payload: { id },
+  type: DELETE_ITEM,
+  payload: { id },
 });
 
 export const enableEditItem = (id) => ({
-  type: ENABLE_EDIT_ITEM, payload: { id },
+  type: ENABLE_EDIT_ITEM,
+  payload: { id },
 });
 
 export const saveChangesToItem = (id, text) => ({
-  type: SAVE_CHANGES_TO_ITEM, payload: { id, text },
+  type: SAVE_CHANGES_TO_ITEM,
+  payload: { id, text },
 });
 
 export const cancelChangesToItem = (id) => ({
-  type: CANCEL_CHANGES_TO_ITEM, payload: { id },
+  type: CANCEL_CHANGES_TO_ITEM,
+  payload: { id },
 });
