bd99c02c4530e5a7fe9dcfbcc47c63069d2d00d2
David Cechak <DavidC@kentico.com>
2017-04-11 13:07:00 +0200

    KA-67 Rewrite List as stateless component

--- onboarding-task/src/components/List.tsx
+++ onboarding-task/src/components/List.tsx
@@ -12,38 +12,29 @@ interface IListCallbacksProps {
   onAddItem: (value: string) => void,
 }
 
-class List extends React.PureComponent<IListDataProps & IListCallbacksProps, undefined> {
-  static displayName = 'List';
+const List: React.StatelessComponent<IListDataProps & IListCallbacksProps> = (props) => {
+  const itemsList = props.itemsOrder.toIndexedSeq().map((id: string) => (
+    <li className="list-group-item" key={id}>
+      <ListItem id={id} />
+    </li>)
+  );
 
-  static propTypes = {
-    itemsOrder: React.PropTypes.instanceOf(Immutable.OrderedSet).isRequired,
-    onAddItem: React.PropTypes.func.isRequired,
-  };
+  return (
+    <div className="col-sm-12 col-md-offset-2 col-md-8">
+      <ul className="list-group">
+        {itemsList}
+        <li className="list-group-item">
+          <AddItem onAdd={props.onAddItem} />
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-  constructor(props: IListDataProps & IListCallbacksProps) {
-    super(props);
-  }
-
-  _renderListItems = () => {
-    return this.props.itemsOrder.toIndexedSeq().map((id: string) =>
-      <li className="list-group-item" key={id}>
-        <ListItem id={id} />
-      </li>
-    );
-  };
-
-  render() {
-    return (
-      <div className="col-sm-12 col-md-offset-2 col-md-8">
-        <ul className="list-group">
-          {this._renderListItems()}
-          <li className="list-group-item">
-            <AddItem onAdd={this.props.onAddItem} />
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+List.displayName = 'List';
+List.propTypes = {
+  itemsOrder: React.PropTypes.instanceOf(Immutable.OrderedSet).isRequired,
+  onAddItem: React.PropTypes.func.isRequired,
+};
 
 export { List, IListDataProps, IListCallbacksProps };
