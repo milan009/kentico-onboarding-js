d8a0bba7596bbb0ca1c8e92cfe889cdb9930f637
Pavel Fojtik <PavelF@kentico.com>
2017-04-11 15:43:54 +0200

    KA-104 - Enable intellisense in item reducer for received item

--- onboarding-task/src/reducers/items.ts
+++ onboarding-task/src/reducers/items.ts
@@ -2,16 +2,19 @@ import { OrderedMap } from 'immutable';
 import { DELETE_ITEM, FETCH_ITEMS_RECEIVE, UPDATE_ITEM, POST_ITEM_RECEIVE } from '../actions/actionTypes';
 import { IAction } from '../actions/IAction';
 import { Item } from '../models/Item';
+import {IItemResponse} from '../models/IItemResponse';
 
 const items = (state = OrderedMap(), action: IAction) => {
   switch (action.type) {
     case FETCH_ITEMS_RECEIVE:
+      const receivedItems: IItemResponse[] = action.payload.items;
       return OrderedMap (
-        action.payload.items.map((item: any) => [item.id, new Item({id: item.id, text: item.text})])
+        receivedItems.map((item: IItemResponse) => [item.id, new Item({id: item.id, text: item.text})])
       );
 
     case POST_ITEM_RECEIVE:
-      return state.set(action.payload.item.id, new Item({id: action.payload.item.id, text: action.payload.item.text}));
+      const receivedItem: IItemResponse = action.payload.item;
+      return state.set(receivedItem.id, new Item({id: receivedItem.id, text: receivedItem.text}));
 
     case DELETE_ITEM:
       return state.delete(action.payload.id);
