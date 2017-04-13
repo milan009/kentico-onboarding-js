66f75db35083c9e8eddf0d15775af1f32acb43b9
Tobias Kamenicky <TobiasK@kentico.com>
2017-04-13 10:40:55 +0200

    KA-127 Add reducer for errors

---
+++ onboarding-task/src/reducers/errorsReducer.ts
@@ -0,0 +1,36 @@
+import { OrderedMap } from 'immutable';
+import { IAction } from '../actions/IAction';
+import {
+  DELETE_ITEM_FAILURE, DELETE_ITEM_SUCCESS,
+  GET_ITEMS_SUCCESS, POST_ITEM_SUCCESS,
+  PUT_ITEM_SUCCESS, PUT_ITEM_FAILURE,
+  POST_ITEM_FAILURE, GET_ITEMS_FAILURE, DISMISS_ERROR
+} from '../actions/actionTypes';
+
+import { generateUuid } from '../utils/idGenerator';
+
+const errorsReducer = (state = OrderedMap<string, string>(), action: IAction): OrderedMap<string, string> => {
+  switch (action.type) {
+    case GET_ITEMS_SUCCESS:
+    case POST_ITEM_SUCCESS:
+    case PUT_ITEM_SUCCESS:
+    case DELETE_ITEM_SUCCESS:
+      return state.clear();
+
+    case GET_ITEMS_FAILURE:
+    case POST_ITEM_FAILURE:
+    case PUT_ITEM_FAILURE:
+    case DELETE_ITEM_FAILURE: {
+      console.log(action);
+      return state.merge(action.payload.errors.map((v: string) => [generateUuid(), v]));
+    }
+
+    case DISMISS_ERROR:
+      return state.delete(action.payload.key);
+
+    default:
+      return state;
+  }
+};
+
+export { errorsReducer };
--- onboarding-task/src/reducers/rootReducer.ts
+++ onboarding-task/src/reducers/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { itemsReducer } from './itemsReducers/itemsReducer';
 import { IAction } from '../actions/IAction';
 import { IAppState } from './IAppState';
+import { errorsReducer } from './errorsReducer';
 
 type IRootReducer = (state: IAppState, action: IAction) => IAppState;
 
 const rootReducer: IRootReducer = combineReducers({
   items: itemsReducer,
+  errors: errorsReducer,
 }) as IRootReducer;
 
 export { rootReducer };
