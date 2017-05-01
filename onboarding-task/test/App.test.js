4414c6f5e028a03bbf107368d4d6e0a331755c31
LindaH32 <LindaH@kentico.com>
2017-05-01 20:43:38 +0200

    KA-112 - Add a comment of why the App test is ignored

--- onboarding-task/test/App.test.js
+++ onboarding-task/test/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/App.jsx';
 
+// This test is ignored because the App element should be wrapped inside the Provider element
 xit('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
