ec799f4a5ba66ef12ddffde7c262ae27d859e658
Milan Jankovic <MilanJ@kentico.com>
2017-06-28 11:48:05 +0200

    KA-146 Add Redux actionTypes

---
+++ onboarding-task/src/actionTypes.js
@@ -0,0 +1,9 @@
+/**
+ * Created by MilanJ on 28.6.2017.
+ */
+
+export const ITEM_CREATED = 'ITEM_CREATED';
+export const ITEM_DELETED = 'ITEM_DELETED';
+export const CHANGE_CANCELLED = 'CHANGE_CANCELLED';
+export const CHANGE_INITIATED = 'CHANGE_INITIATED';
+export const CHANGE_SAVED = 'CHANGE_SAVED';
