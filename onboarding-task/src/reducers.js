49f06fc5c6240ad12b1166d655fe4688ab1611d1
Milan Jankovic <MilanJ@kentico.com>
2017-07-04 09:22:07 +0200

    KA-146 Refactor reducers to use mergeIn instead of set

--- onboarding-task/src/reducers.js
+++ onboarding-task/src/reducers.js
@@ -7,14 +7,13 @@ import uuidV4 from 'uuid';
 import * as ActionTypes from './actionTypes';
 import { Item } from './models/Item';
 
-const addItemReducer = (state = new OrderedMap(), action) => {
+const createItemReducer = (state = new OrderedMap(), action) => {
   switch (action.type) {
     case ActionTypes.ITEM_CREATED: {
       const newItem = new Item({
         id: uuidV4(),
         text: action.text,
       });
-
       return state.set(newItem.id, newItem);
     }
 
@@ -36,26 +35,25 @@ const deleteItemReducer = (state = new OrderedMap(), action) => {
 const changeItemReducer = (state = new OrderedMap(), action) => {
   switch (action.type) {
     case ActionTypes.MAKE_EDITABLE:
-      return state.set(action.id,
-        new Item({
-          id: action.id,
-          text: action.text,
-          isEdited: true,
-        }));
+      return state.mergeIn(
+        [action.id],
+        { isEdited: true },
+      );
 
     case ActionTypes.CHANGE_CANCELLED:
-      return state.set(action.id,
-        new Item({
-          id: action.id,
-          text: action.text,
-        }));
+      return state.mergeIn(
+        [action.id],
+        { isEdited: false },
+      );
 
     case ActionTypes.CHANGE_SAVED:
-      return state.set(action.id,
-        new Item({
-          id: action.id,
+      return state.mergeIn(
+        [action.id],
+        {
           text: action.text,
-        }));
+          isEdited: false,
+        },
+      );
 
     default:
       return state;
@@ -63,7 +61,7 @@ const changeItemReducer = (state = new OrderedMap(), action) => {
 };
 
 export {
-  addItemReducer,
+  createItemReducer,
   deleteItemReducer,
   changeItemReducer,
 };
