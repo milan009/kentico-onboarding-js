0d38831dfd5a4cc8390ba6c18d098dc0ef1dddc9
Milan Jankovic <MilanJ@kentico.com>
2017-07-04 10:44:00 +0200

    KA-146 Create combined root reducer

--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, /* combineReducers,*/ applyMiddleware } from 'redux';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux';
 import { logger } from 'redux-logger';
 import { Provider } from 'react-redux';
 
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import * as Reducers from './reducers';
+import * as Reducers from './reducers/itemReducers';
 
-/* const reducer = combineReducers({
-  changeItemReducer: Reducers.changeItemReducer,
-  addItemReducer: Reducers.createItemReducer,
-  deleteItemReducer: Reducers.deleteItemReducer,
-}); */
-
-const reducer = (state, action) => {
-  let resState = Reducers.changeItemReducer(state, action);
-  resState = Reducers.deleteItemReducer(resState, action);
-  resState = Reducers.createItemReducer(resState, action);
-  return resState;
-};
+const reducer = combineReducers({
+  items: Reducers.itemsReducer,
+});
 
 const store = createStore(reducer, applyMiddleware(logger));
 
