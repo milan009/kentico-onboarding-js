2df41362483a44c23b2a4fb311c4f1ddc2dd2a93
Milan Jankovic <MilanJ@kentico.com>
2017-08-01 14:51:09 +0200

    KA-146 Add unit tests for memoization in ViewItem model

---
+++ onboarding-task/test/models/ViewItem.test.js
@@ -0,0 +1,30 @@
+import { ViewItem } from '../../src/models/ViewItem';
+
+describe('View item memization tests', () => {
+  it('creates correct ViewItem object', () => {
+    const mockId = '12345678-0000-0000-0000-000000000000';
+    const mockDataObject = { text: 'Mlock' };
+    const mockFlagsObject = { isBeingEdited: false };
+    const expectedViewItem = {
+      id: mockId,
+      index: 2,
+      text: 'Mlock',
+      isBeingEdited: false,
+    };
+
+    const createdViewItem = ViewItem(mockId, 2, mockDataObject, mockFlagsObject);
+
+    expect(createdViewItem).toEqual(expectedViewItem);
+  });
+
+  it('multiple calls with same parameters return same reference', () => {
+    const mockId = '12345678-0000-0000-0000-000000000000';
+    const mockDataObject = { text: 'Mlock' };
+    const mockFlagsObject = { isBeingEdited: false };
+
+    const firstViewItem = ViewItem(mockId, 2, mockDataObject, mockFlagsObject);
+    const secondViewItem = ViewItem(mockId, 2, mockDataObject, mockFlagsObject);
+
+    expect(firstViewItem).toBe(secondViewItem);
+  });
+});
