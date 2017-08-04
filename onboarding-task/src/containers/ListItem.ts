ad406c82b5d51426eb4143fed2da9d52d7a76371
Milan Jankovic <MilanJ@kentico.com>
2017-08-04 13:39:31 +0200

    KA-147 Rewrite ListItem container into TypeScript

---
+++ onboarding-task/src/containers/ListItem.ts
@@ -0,0 +1,33 @@
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+
+import { ListItem as ListItemComponent, IListItemDataProps, IListItemCallbackProps } from '../components/ListItem';
+import * as actionCreators from '../actions/actionCreators';
+import { ViewItem } from '../models/ViewItem';
+import { IStore } from '../reducers/rootReducer';
+
+interface IListItemContainerProps {
+  id: string;
+  index: number;
+}
+
+const mapStateToProps = (state: IStore, { id, index }: IListItemContainerProps): IListItemDataProps => ({
+  item: ViewItem(
+    index,
+    state.list.itemsById.get(id),
+    state.list.itemFlagsMap.get(id),
+  ),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<IStore>, { id }: IListItemContainerProps): IListItemCallbackProps => ({
+  onClick: () =>
+    dispatch(actionCreators.makeEditable(id)),
+  onDelete: () =>
+    dispatch(actionCreators.deleteItem(id)),
+  onSave: (newText: string) =>
+    dispatch(actionCreators.saveChange(id, newText)),
+  onCancel: () =>
+    dispatch(actionCreators.cancelChange(id)),
+});
+
+export const ListItem: React.ComponentClass<IListItemContainerProps> = connect(mapStateToProps, mapDispatchToProps)(ListItemComponent);
