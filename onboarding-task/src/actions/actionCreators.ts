e8defd63876e01229ecafdc19f637b866392e19f
Milan Jankovic <MilanJ@kentico.com>
2017-09-14 14:23:28 +0200

    KA-197 Implement thunks for remaining HTTP methods
    Thunks for PUT, POST and DELETE have been implemented, along with actions that signify the beginning, success and failure of given http request.
    
    Action types for those actions have also been created.

--- onboarding-task/src/actions/actionCreators.ts
+++ onboarding-task/src/actions/actionCreators.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from 'redux';
 import * as uuidV4 from 'uuid';
 import { Promise } from 'es6-promise';
+import { OrderedMap } from 'immutable';
 
 import { IAction } from '../interfaces/IAction';
 import {
@@ -9,17 +10,32 @@ import {
   ITEM_CREATED,
   ITEM_DELETED,
   ITEM_MAKE_EDITABLE,
+
   FETCH_STARTED,
   FETCH_SUCCESS,
   FETCH_FAIL,
+
+  POST_REQUEST_FAIL,
+  POST_REQUEST_STARTED,
+  POST_REQUEST_SUCCESS,
+
+  DELETE_REQUEST_FAIL,
+  DELETE_REQUEST_SUCCESS,
+  DELETE_REQUEST_STARTED,
+
+  PUT_REQUEST_FAIL,
+  PUT_REQUEST_SUCCESS,
+  PUT_REQUEST_STARTED,
+
   PARSE_RESPONSE_FINISHED,
   PARSE_RESPONSE_STARTED,
 } from './actionTypes';
 import { IStore } from '../interfaces/IStore';
 
 import { ItemsDataMap } from '../reducers/list/itemsReducer';
-import { OrderedMap } from 'immutable';
 import { ItemData } from '../models/ItemData';
+
+// region Frontend related action creators
 
 export const createItemFactory = (idGenerator: () => string): (text: string) => IAction => (
   (text: string) => ({
@@ -57,6 +73,10 @@ export const makeEditable = (id: string): IAction => ({
   payload: {id},
 });
 
+// endregion
+
+// region GET related action creators
+
 export const startFetchingItems = (): IAction => ({
   type: FETCH_STARTED,
 });
@@ -89,6 +109,83 @@ export const parsingFinished = (parsedItems: ItemsDataMap): IAction => ({
   }
 });
 
+// endregion
+
+// region POST related action creators
+
+export const postStarted = (text: string): IAction => ({
+  type: POST_REQUEST_STARTED,
+  payload: {
+    text,
+  }
+});
+
+export const postSucceeded = (json: any): IAction => ({
+  type: POST_REQUEST_SUCCESS,
+  payload: {
+    item: json,
+  },
+});
+
+export const postFailed = (error: string): IAction => ({
+  type: POST_REQUEST_FAIL,
+  payload: {
+    error
+  }
+});
+
+// endregion
+
+// region PUT related action creators
+
+export const putStarted = (item: ItemData): IAction => ({
+  type: PUT_REQUEST_STARTED,
+  payload: {
+    item,
+  }
+});
+
+export const putSucceeded = (json: any): IAction => ({
+  type: PUT_REQUEST_SUCCESS,
+  payload: {
+    item: json,
+  },
+});
+
+export const putFailed = (error: string): IAction => ({
+  type: PUT_REQUEST_FAIL,
+  payload: {
+    error
+  }
+});
+
+// endregion
+
+// region DELETE related action creators
+
+export const deleteStarted = (id: string): IAction => ({
+  type: DELETE_REQUEST_STARTED,
+  payload: {
+    id,
+  }
+});
+
+export const deleteSucceeded = (id: string): IAction => ({
+  type: DELETE_REQUEST_SUCCESS,
+  payload: {
+    id
+  },
+});
+
+export const deleteFailed = (error: string): IAction => ({
+  type: DELETE_REQUEST_FAIL,
+  payload: {
+    error
+  }
+});
+
+// endregion
+
 const parseAPIResponseJson = (json: any) => {
   return new Promise<ItemsDataMap>((resolve) => {
     let parsedItems = OrderedMap<string, ItemData>();
@@ -104,13 +201,102 @@ const parseAPIResponseJson = (json: any) => {
   });
 };
 
+// region THUNKS
 export const parseItems = (json: any) => {
-    return function (dispatch: Dispatch<IStore>) {
-      dispatch(parsingStarted(json));
+  return function (dispatch: Dispatch<IStore>) {
+    dispatch(parsingStarted(json));
 
-      return parseAPIResponseJson(json)
-        .then((parsedItems: any) => dispatch(parsingFinished(parsedItems)));
+    return parseAPIResponseJson(json)
+      .then((parsedItems: any) => dispatch(parsingFinished(parsedItems)));
+  };
+};
+
+export const putSavedItem = (item: ItemData) => {
+  return function (dispatch: any) {
+    const headers = new Headers();
+    headers.append('Content-type', 'Application/json');
+
+    const init = {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify({Id: item.id, Text: item.text}),
     };
+
+    dispatch(putStarted(item));
+
+    return fetch(`api/v1/items/${item.id}`, init)
+      .then((response) => {
+        if (!response.ok) {
+          return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+        }
+        return response.json();
+      })
+      .then(
+        (json) => {
+          dispatch(putSucceeded(json));
+          return json;
+        },
+        (error) => dispatch(putFailed(error)))
+      .then((receivedItem: any) => dispatch(saveChange(receivedItem.Id, receivedItem.Text)));
+  };
+};
+
+export const postNewItem = (newText: string) => {
+  return function (dispatch: any) {
+    const headers = new Headers();
+    headers.append('Content-type', 'Application/json');
+
+    const guid = '00000000-0000-0000-0000-000000000000';
+
+    const init = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({Id: guid, Text: newText}),
+    };
+
+    dispatch(postStarted(newText));
+
+    return fetch(`api/v1/items`, init)
+      .then((response) => {
+        if (!response.ok) {
+          return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+        }
+        return response.json();
+      })
+      .then(
+        (json) => {
+          dispatch(postSucceeded(json));
+          return json.Id;
+        },
+        (error) => dispatch(postFailed(error)))
+      .then((id) => dispatch(createItemFactory(() => id)(newText)));
+  };
+};
+
+export const deleteStoredItem = (id: string) => {
+  return function (dispatch: any) {
+    const headers = new Headers();
+    const init = {
+      method: 'DELETE',
+      headers,
+    };
+
+    dispatch(deleteStarted(id));
+
+    return fetch(`api/v1/items/${id}`, init)
+      .then((response) => {
+        if (!response.ok) {
+          return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+        }
+        return response;
+      })
+      .then(
+        () => {
+          dispatch(deleteSucceeded(id));
+          return dispatch(deleteItem(id));
+        },
+        (error) => dispatch(deleteFailed(error)));
+  };
 };
 
 export const fetchItems = () => {
@@ -131,3 +317,4 @@ export const fetchItems = () => {
       ).then((parsedItems) => dispatch(fetchingSucceeded(parsedItems)));
   };
 };
+// endregion
