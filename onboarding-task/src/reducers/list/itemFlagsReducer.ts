8d5b30351163bfa4bf4c15777f26b91855307cf9
Milan Jankovic <MilanJ@kentico.com>
2017-11-23 10:19:19 +0100

    KA-197 Finish rewriting reducer tests
    All actions supported by reducers that were at least somewhat covered by tests should now be tested. New reducers still don't have any tests.

--- onboarding-task/src/reducers/list/itemFlagsReducer.ts
+++ onboarding-task/src/reducers/list/itemFlagsReducer.ts
@@ -1,11 +1,14 @@
 import { ItemFlags } from '../../models/ItemFlags';
 import { IAction } from '../../interfaces/IAction';
 import {
-  DELETE_REQUEST_FAIL, DELETE_REQUEST_STARTED,
+  DELETE_REQUEST_FAIL,
+  DELETE_REQUEST_STARTED,
   ITEM_CHANGE_CANCELLED,
-  ITEM_MAKE_EDITABLE, POST_REQUEST_FAIL, POST_REQUEST_STARTED, POST_REQUEST_SUCCESS,
+  ITEM_MAKE_EDITABLE,
+  POST_REQUEST_FAIL,
   PUT_REQUEST_FAIL,
-  PUT_REQUEST_STARTED, PUT_REQUEST_SUCCESS,
+  PUT_REQUEST_STARTED,
+  PUT_REQUEST_SUCCESS,
 } from '../../actions/actionTypes';
 
 const defaultState = new ItemFlags();
@@ -20,21 +23,18 @@ export const itemFlagsReducer = (state: ItemFlags = defaultState, action: IActio
 
     case DELETE_REQUEST_STARTED:
     case PUT_REQUEST_STARTED:
-    case POST_REQUEST_STARTED:
       return state.typedMerge({isBeingEdited: false, isStored: false, requestError: null});
 
     case PUT_REQUEST_SUCCESS:
-    case POST_REQUEST_SUCCESS: {
-      return state.typedMerge({isStored: true, isBeingEdited: false} );
-    }
+      return state.typedMerge({isStored: true, isBeingEdited: false});
 
     case PUT_REQUEST_FAIL:
     case DELETE_REQUEST_FAIL: {
-      return state.typedMerge({isStored: true, requestError: action.payload});
+      return state.typedMerge(new ItemFlags({isStored: true, requestError: action.payload}));
     }
 
     case POST_REQUEST_FAIL: {
-      return state.typedMerge({isStored: false, requestError: action.payload});
+      return state.typedMerge(new ItemFlags({isStored: false, requestError: action.payload}));
     }
 
     default:
