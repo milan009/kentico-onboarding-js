414166390fe9920eaffbcc558d048152d5eb9a05
Milan Jankovic <MilanJ@kentico.com>
2018-01-10 10:31:44 +0100

    KA-187 Setup urls and proxy for production

--- onboarding-task/src/utils/constants.ts
+++ onboarding-task/src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const emptyUuid = '00000000-0000-0000-0000-000000000000';
-export const controllerUrl = 'api/v1/items';
+export const controllerUrl = 'https://kaonboardinglistapp.azurewebsites.net/api/v1/items';
